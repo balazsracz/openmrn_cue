0664c5b584fc45c919626fdfb3f05b2adef2047d
Balazs Racz <bracz@google.com>
2013-06-28 21:54:36 +0200

    reformats variables.hxx
    Fixes bug in ID set being offset by two.
    Updates test accordingly.

--- automata/variables.hxx
+++ automata/variables.hxx
@@ -8,51 +8,58 @@ namespace automata {
 
 /**
    A global variable implementation that uses two events to set a state bit.
- */
+*/
 class EventBasedVariable : public GlobalVariable {
-public:
-    EventBasedVariable(Board* brd,
-                       uint64_t event_on, uint64_t event_off,
-                       int client, int offset, int bit)
-        : event_on_(event_on), event_off_(event_off) {
-        arg1_ = (0 << 5) | (client & 0b11111);
-        arg2_ = (offset << 3) | (bit & 7);
-        if (brd) {
-            brd->AddVariable(this);
-        }
+ public:
+  EventBasedVariable(Board* brd,
+                     uint64_t event_on, uint64_t event_off,
+                     int client, int offset, int bit)
+      : event_on_(event_on), event_off_(event_off) {
+    arg1_ = (0 << 5) | (client & 0b11111);
+    arg2_ = (offset << 3) | (bit & 7);
+    if (brd) {
+      brd->AddVariable(this);
     }
-    virtual ~EventBasedVariable() {}
+  }
+  virtual ~EventBasedVariable() {}
 
-    virtual void Render(string* output) {
-        typedef Automata::Op Op;
-        vector<uint8_t> empty;
-        vector<uint8_t> op;
-        op.push_back(_ACT_SET_EVENTID);
-        op.push_back(0b01010111);
-        for (int b = 56; b >= 0; b -= 8) {
-            op.push_back((event_on_ >> b) & 0xff);
-        }
-        Op::CreateOperation(output, empty, op);
-        // TODO(bracz): here we should optimize the used bytes count.
-        op.clear();
-        op.push_back(_ACT_SET_EVENTID);
-        op.push_back(0b00000111);
-        for (int b = 56; b >= 0; b -= 8) {
-            op.push_back((event_off_ >> b) & 0xff);
-        }
-        Op::CreateOperation(output, empty, op);
-        op.clear();
-        op.push_back(_ACT_DEF_VAR);
-        op.push_back(arg1_);
-        op.push_back(arg2_);
-        SetId(output->size());
-        Op::CreateOperation(output, empty, op);
+  // Creates an operation setting a particular eventid.
+  //
+  // dst is the index of eventid to set (0 or 1).
+  static void CreateEventId(int dst, uint64_t eventid, string* output) {
+    vector<uint8_t> empty;
+    vector<uint8_t> op;
+    op.push_back(_ACT_SET_EVENTID);
+    if (dst) {
+      op.push_back(0b01010111);
+    } else {
+      op.push_back(0b00000111);
     }
+    for (int b = 56; b >= 0; b -= 8) {
+      op.push_back((eventid >> b) & 0xff);
+    }
+    Automata::Op::CreateOperation(output, empty, op);
+  }
 
-private:
-    uint64_t event_on_, event_off_;
-    uint8_t arg1_;
-    uint8_t arg2_;
+  virtual void Render(string* output) {
+    CreateEventId(1, event_on_, output);
+    CreateEventId(0, event_off_, output);
+
+    // We take the lead byte of op and the _ACT_DEF_VAR byte into account.
+    SetId(output->size() + 2);
+
+    vector<uint8_t> empty;
+    vector<uint8_t> op;
+    op.push_back(_ACT_DEF_VAR);
+    op.push_back(arg1_);
+    op.push_back(arg2_);
+    Automata::Op::CreateOperation(output, empty, op);
+  }
+
+ private:
+  uint64_t event_on_, event_off_;
+  uint8_t arg1_;
+  uint8_t arg2_;
 };
 
 }  // namespace
