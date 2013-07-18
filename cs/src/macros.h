6ecaac7be574c72ff501773114d3b00e068040a9
Balazs Racz <balazs.racz@gmail.com>
2013-07-18 12:05:57 +0200

    Header file for utility macros.

---
+++ cs/src/macros.h
@@ -0,0 +1,11 @@
+
+#ifndef _BRACZ_TRAIN_MACROS_H_
+#define _BRACZ_TRAIN_MACROS_H_
+
+
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);   \
+  void operator=(const TypeName&)
+
+
+#endif  //_BRACZ_TRAIN_MACROS_H_
