b3e0f88f34258915a12a06baf9c6d37e44a2fcf3
Balazs Racz <balazs.racz@gmail.com>
2013-07-21 14:18:48 +0200

    empty template file for signaling board.

---
+++ automata/automata_templates.cpp
@@ -0,0 +1,30 @@
+
+#include "operations.hxx"
+#include "system.hxx"
+
+using namespace automata;
+
+
+#define ANONYMOUS_NAMESPACE namespace {
+#define ANONYMOUS_NAMESPACE_END }
+
+ANONYMOUS_NAMESPACE
+
+StateRef StInit(0);
+StateRef StBase(1);
+StateRef StGoing(2);
+
+
+class SignalingBoardAutomata : public automata::Automata {
+public:
+  
+
+
+protected:
+
+
+};
+
+
+
+ANONYMOUS_NAMESPACE_END
