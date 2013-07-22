60c7673bd52b9d23334f814458c5defc35ff7984
Balazs Racz <bracz@google.com>
2013-07-22 23:04:56 +0200

    LPC11Cxx optimizations: * removes two library function calls, __cxa_virtual (which contributes 50K unused flash code due to collateral linking) and __cxa_atexit, which allocates some 400 bytes of heap at startup to run global destructors (which never will run in an MCU) * pulls in optimized resource definitions (less stack space mostly) * opens the physical CAN device * makes the I2C updater run in the main thread * adds a couple of I2C event listeners

--- cs/main.cxx
+++ cs/main.cxx
@@ -62,6 +62,23 @@
 const char *nmranet_manufacturer = "Balazs Racz";
 const char *nmranet_hardware_rev = "N/A";
 const char *nmranet_software_rev = "0.1";
+
+
+#ifdef TARGET_LPC11Cxx
+// Memory constrained situation -- set the parameters to tight values.
+const size_t main_stack_size = 900;
+const int main_priority = 0;
+const size_t ALIAS_POOL_SIZE = 1;
+const size_t DOWNSTREAM_ALIAS_CACHE_SIZE = 1;
+const size_t UPSTREAM_ALIAS_CACHE_SIZE = 1;
+const size_t DATAGRAM_POOL_SIZE = 0;
+const size_t CAN_RX_BUFFER_SIZE = 1;
+const size_t CAN_TX_BUFFER_SIZE = 8;
+const size_t SERIAL_RX_BUFFER_SIZE = 16;
+const size_t SERIAL_TX_BUFFER_SIZE = 16;
+const size_t DATAGRAM_THREAD_STACK_SIZE = 256;
+const size_t CAN_IF_READ_THREAD_STACK_SIZE = 700;
+#else
 const size_t main_stack_size = 2560;
 const int main_priority = 0;
 const size_t ALIAS_POOL_SIZE = 2;
@@ -74,8 +91,10 @@ const size_t SERIAL_RX_BUFFER_SIZE = 128;
 const size_t SERIAL_TX_BUFFER_SIZE = 130;
 const size_t DATAGRAM_THREAD_STACK_SIZE = 512;
 const size_t CAN_IF_READ_THREAD_STACK_SIZE = 1024;
+#endif
 
 extern const unsigned long long NODE_ADDRESS;
+
 
 extern "C" {
 void resetblink(uint32_t pattern);
@@ -83,9 +102,19 @@ void diewith(uint32_t pattern);
 extern uint32_t blinker_pattern;
 
 #ifdef TARGET_LPC11Cxx
+  // This gets rid of about 50 kbytes of flash code that is unnecessarily
+  // linked into the binary.
 void __wrap___cxa_pure_virtual(void) {
   abort();
 }
+
+  // This removes 400 bytes of memory allocated at startup for the atexit
+  // structure.
+int __wrap___cxa_atexit(void) {
+  return 0;
+}
+
+
 #endif
 
 
@@ -131,11 +160,41 @@ public:
 BlinkerToggleEventHandler led_blinker(0x0502010202650012ULL,
                                       0x0502010202650013ULL);
 
+#ifdef TARGET_LPC11Cxx
+MemoryToggleEventHandler<uint8_t> l1(0x0502010202650040ULL,
+                                     0x0502010202650041ULL,
+                                     1,
+                                     get_state_byte(1, OFS_IOA));
+
+/*MemoryToggleEventHandler<uint8_t> l2(0x0502010202650042ULL,
+                                     0x0502010202650043ULL,
+                                     0,
+                                     get_state_byte(1, OFS_IOA));
+*/
+MemoryToggleEventHandler<uint8_t> l3(0x0502010202650044ULL,
+                                     0x0502010202650045ULL,
+                                     1,
+                                     get_state_byte(2, OFS_IOA));
+
+/*MemoryToggleEventHandler<uint8_t> l4(0x0502010202650046ULL,
+                                     0x0502010202650047ULL,
+                                     0,
+                                     get_state_byte(2, OFS_IOA));*/
+#endif
+
 
 #if defined(TARGET_LPC2368)
 DECLARE_PIPE(can_pipe0);
 DECLARE_PIPE(can_pipe1);
 #endif
+
+
+extern "C" {
+void appl_hw_init(void) __attribute__ ((weak));
+void appl_hw_init(void) {
+}
+
+}
 
 /** Entry point to application.
  * @param argc number of command line arguments
@@ -144,6 +203,8 @@ DECLARE_PIPE(can_pipe1);
  */
 int appl_main(int argc, char *argv[])
 {
+  appl_hw_init();
+
 #if defined(TARGET_LPC2368)
     start_watchdog(2000);
     add_watchdog_reset_timer(500);
@@ -151,20 +212,29 @@ int appl_main(int argc, char *argv[])
 
 #if defined(TARGET_LPC2368)
     PacketQueue::initialize("/dev/serUSB0");
+    can_pipe0.AddPhysicalDeviceToPipe("/dev/can1", "can0_rx_thread", 512);
 #endif
 
-    //can_pipe0.AddPhysicalDeviceToPipe("/dev/can0", "can0_rx_thread", 512);
     //can_pipe0.AddPhysicalDeviceToPipe("/dev/can1", "can1_rx_thread", 512);
 
 
-#if defined(TARGET_LPC2368) || defined(__linux__)
+    NMRAnetIF *nmranet_if;
+
+#if defined(TARGET_LPC2368x) || defined(__linux__)
     int fd = open("/dev/canp1v0", O_RDWR);
     ASSERT(fd >= 0);
     dcc_can_init(fd);
 #endif
 
-    NMRAnetIF *nmranet_if;
+
+#if defined(TARGET_LPC2368) || defined(__linux__)
     nmranet_if = nmranet_can_if_init(NODE_ADDRESS, "/dev/canp0v1", read, write);
+#elif defined(TARGET_LPC11Cxx)
+    nmranet_if = nmranet_can_if_init(NODE_ADDRESS, "/dev/can0", read, write);
+#else
+    #error define where to open the can bus.
+#endif
+
 
 
     if (nmranet_if == NULL)
@@ -179,26 +249,58 @@ int appl_main(int argc, char *argv[])
     node = nmranet_node_create(NODE_ADDRESS, nmranet_if, "Virtual Node", NULL);
     nmranet_node_user_description(node, "Test Node");
 
-    nmranet_event_consumer(node, 0x0502010202000000ULL, EVENT_STATE_INVALID);
+    //  nmranet_event_consumer(node, 0x0502010202000000ULL, EVENT_STATE_INVALID);
     nmranet_event_consumer(node, 0x0502010202650013ULL, EVENT_STATE_INVALID);
     nmranet_event_consumer(node, 0x0502010202650012ULL, EVENT_STATE_INVALID);
-    nmranet_event_consumer(node, 0x05020102a8650013ULL, EVENT_STATE_INVALID);
-    nmranet_event_consumer(node, 0x05020102a8650012ULL, EVENT_STATE_INVALID);
-    nmranet_event_producer(node, 0x0502010202000000ULL, EVENT_STATE_INVALID);
+    /*    nmranet_event_consumer(node, 0x05020102a8650013ULL, EVENT_STATE_INVALID);
+          nmranet_event_consumer(node, 0x05020102a8650012ULL, EVENT_STATE_INVALID);*/
+    nmranet_event_consumer(node, 0x0502010202650040ULL, EVENT_STATE_INVALID);
+    nmranet_event_consumer(node, 0x0502010202650041ULL, EVENT_STATE_INVALID);
+    nmranet_event_consumer(node, 0x0502010202650044ULL, EVENT_STATE_INVALID);
+    nmranet_event_consumer(node, 0x0502010202650045ULL, EVENT_STATE_INVALID);
+    /*    nmranet_event_producer(node, 0x0502010202000000ULL, EVENT_STATE_INVALID);
     nmranet_event_producer(node, 0x0502010202650012ULL, EVENT_STATE_INVALID);
-    nmranet_event_producer(node, 0x0502010202650013ULL, EVENT_STATE_VALID);
+    nmranet_event_producer(node, 0x0502010202650013ULL, EVENT_STATE_VALID);*/
     nmranet_node_initialized(node);
 
     //os_thread_create(NULL, "out_blinker", 0, 800, out_blinker_thread, NULL);
-#ifdef __FreeRTOS__
-    i2c_updater.RunInNewThread("i2c_update", 0, 1024);
-#endif
 
 
 #if defined(TARGET_LPC2368) || defined(__linux__)
     AutomataRunner runner(node, (insn_t*)0x78000);
     resume_all_automata();
 #endif
+
+#if defined(TARGET_LPC2368)
+    i2c_updater.RunInNewThread("i2c_update", 0, 1024);
+#endif
+
+    resetblink(0);
+
+#ifdef __FreeRTOS__
+    for (;;)
+    {
+
+#if !defined(TARGET_LPC2368)
+      // This takes a bit of time (blocking).
+      i2c_updater.Step();
+#else
+      nmranet_node_wait(node, MSEC_TO_NSEC(300));
+#endif
+
+      // These are quick.
+      for (size_t i = nmranet_event_pending(node); i > 0; i--) {
+        node_handle_t node_handle;
+        uint64_t event = nmranet_event_consume(node, &node_handle);
+        registry.HandleEvent(event);
+      }
+      for (size_t i = nmranet_datagram_pending(node); i > 0; i--) {
+        datagram_t datagram = nmranet_datagram_consume(node);
+        // We release unknown datagrams iwthout processing them.
+        nmranet_datagram_release(datagram);
+      }
+    }
+#else
 
     for (;;)
     {
@@ -217,6 +319,11 @@ int appl_main(int argc, char *argv[])
             }
         }
     }
+
+
+#endif
+
+
 
     return 0;
 }
