a3aac4ff2df36027897c51a9419caad6ce6cd056
Balazs Racz <bracz@google.com>
2013-07-22 22:46:09 +0200

    Shrinks the memory usage of the automata control volatile storage for LPC11Cxx, because there will be no actual automata running.

--- cs/src/automata_control.cpp
+++ cs/src/automata_control.cpp
@@ -18,7 +18,12 @@ void resume_all_automata() { automata_on_ = true; }
 //! Returns true if automata processing is enabled.
 bool automata_running() { return automata_on_; }
 
+#ifdef TARGET_LPC11Cxx
+uint8_t clients[3][8];
+#else
 uint8_t clients[8][32];
+#endif
+
 
 //! Returns a state byte from a particular client and offset. Offset is
 //! interpreted in the old system.
@@ -26,7 +31,11 @@ uint8_t clients[8][32];
 //	    if (arg == ((comms_get_address()) << 5) + OFS_GLOBAL_BITS) {
 //          statepacket[i+3] = global_bits[comms_get_address()];
 uint8_t* get_state_byte(int client, int offset) {
+#ifdef TARGET_LPC11Cxx
+    return &(clients[client][offset - 24]);
+#else
     return &(clients[client][offset]);
+#endif
 }
 
 // Array holding the mapping of train location -> train ID.
