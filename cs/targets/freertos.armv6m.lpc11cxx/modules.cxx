1052df8b6763eac95fb43c452af488fc36e70285
Balazs Racz <bracz@google.com>
2013-07-29 19:59:02 +0200

    Updates I2C address of 11Cxx target

--- cs/targets/freertos.armv6m.lpc11cxx/modules.cxx
+++ cs/targets/freertos.armv6m.lpc11cxx/modules.cxx
@@ -20,10 +20,10 @@ VIRTUAL_DEVTAB_ENTRY(canp0v1, can_pipe0, "/dev/canp0v1", 16);
 I2C i2c(P0_5, P0_4);
 
 I2COutUpdater extender0(&i2c, 0x25, {}, get_state_byte(1, OFS_IOA), 2);
-I2COutUpdater extender1(&i2c, 0x24, {}, get_state_byte(2, OFS_IOA), 2);
+I2COutUpdater extender1(&i2c, 0x26, {}, get_state_byte(2, OFS_IOA), 2);
 
 I2CInUpdater in_extender0(&i2c, 0x25, {}, get_state_byte(1, OFS_IOB), 1);
-I2CInUpdater in_extender1(&i2c, 0x24, {}, get_state_byte(2, OFS_IOB), 1);
+I2CInUpdater in_extender1(&i2c, 0x26, {}, get_state_byte(2, OFS_IOB), 1);
 
 SynchronousUpdater i2c_updater({&extender0, &extender1, &in_extender0, &in_extender1});
 
