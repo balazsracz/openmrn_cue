554c5ccc1dd782d873a7c45a3ab197f377ad6917
Balazs Racz <bracz@google.com>
2014-05-12 14:45:36 +0200

    Fixes indexing bug in host logging.

--- cs/custom/HostLogging.cxx
+++ cs/custom/HostLogging.cxx
@@ -7,8 +7,8 @@ namespace bracz_custom {
 
 void send_host_log_event(HostLogEvent e) {
   PacketBase pkt(2);
-  pkt[1] = CMD_CAN_LOG;
-  pkt[2] = (uint8_t)e;
+  pkt[0] = CMD_CAN_LOG;
+  pkt[1] = (uint8_t)e;
   PacketQueue::instance()->TransmitPacket(pkt);
 }
 
