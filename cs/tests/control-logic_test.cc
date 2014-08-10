3832dd33d1ec6b716676ec6312bfbcac08ef9b33
Balazs Racz <balazs.racz@gmail.com>
2014-08-10 19:34:22 +0200

    Adds a test where the train runs in circles but sets the turnout itself.

--- cs/tests/control-logic_test.cc
+++ cs/tests/control-logic_test.cc
@@ -1832,4 +1832,51 @@ TEST_F(SampleLayoutLogicTrainTest, RunCircles) {
   }
 }
 
+TEST_F(SampleLayoutLogicTrainTest, RunCirclesWithTurnout) {
+  class MyTrain : public TrainSchedule {
+   public:
+    MyTrain(SampleLayoutLogicTrainTest* t, Board* b,
+            EventBlock::Allocator* alloc)
+        : TrainSchedule("mytrain", b,
+                        nmranet::TractionDefs::NODE_ID_DCC | 0x1384,
+                        alloc->Allocate("mytrain.pbits", 8),
+                        alloc->Allocate("mytrain", 8)),
+          t_(t) {}
+
+    void RunTransition(Automata* aut) OVERRIDE {
+      AddEagerBlockSequence({&t_->TopA.b, &t_->TopB.b});
+      AddEagerBlockTransition(&t_->TopB.b, &t_->RRight.b);
+      SwitchTurnout(t_->RStubEntry.b.magnet(), true);
+      AddEagerBlockSequence(
+          {&t_->RRight.b, &t_->BotA.b, &t_->BotB.b, &t_->RLeft.b, &t_->TopA.b});
+    }
+   private:
+    SampleLayoutLogicTrainTest* t_;
+  } my_train(this, &brd, alloc());
+  SetupRunner(&brd);
+  Run(20);
+  vector<TestBlock*> blocks = {&TopA, &TopB, &RRight, &BotA, &BotB, &RLeft};
+  TopA.inverted_detector.Set(false);
+  SetVar(*my_train.TEST_GetPermalocBit(&TopA.b), true);
+  
+  for (int i = 0; i < 37; ++i) {
+    TestBlock* cblock = blocks[i % blocks.size()];
+    TestBlock* nblock = blocks[(i + 1) % blocks.size()];
+    Run(20);
+    EXPECT_TRUE(QueryVar(cblock->b.route_out()));
+    EXPECT_TRUE(QueryVar(nblock->b.route_in()));
+    cblock->inverted_detector.Set(true);
+    Run(20);
+    EXPECT_FALSE(QueryVar(cblock->b.route_in()));
+    EXPECT_FALSE(QueryVar(cblock->b.route_out()));
+    EXPECT_TRUE(QueryVar(nblock->b.route_in()));
+
+    EXPECT_TRUE(QueryVar(*my_train.TEST_GetPermalocBit(&nblock->b)));
+    EXPECT_FALSE(QueryVar(*my_train.TEST_GetPermalocBit(&cblock->b)));
+
+    nblock->inverted_detector.Set(false);
+    Run(20);
+  }
+}
+
 }  // namespace automata
