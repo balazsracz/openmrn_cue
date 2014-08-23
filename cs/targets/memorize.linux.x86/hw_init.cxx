98e7a43b81866c6c3c451c70962da3afa8d8d6a6
Balazs Racz <bracz@google.com>
2014-08-24 00:05:45 +0200

    Adds a new binary that runs on linux and memorizes the state of the permabits.

---
+++ cs/targets/memorize.linux.x86/hw_init.cxx
@@ -0,0 +1,4 @@
+#include "can_frame.h"
+
+//extern const unsigned long long NODE_ADDRESS;
+//const unsigned long long NODE_ADDRESS = 0x050101011430ULL;
---
+++ cs/targets/memorize.linux.x86/main.cxx
@@ -0,0 +1,91 @@
+/** \copyright
+ * Copyright (c) 2013, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file main.cxx
+ *
+ * A simple application to demonstrate asynchronous interfaces.
+ *
+ * @author Balazs Racz
+ * @date 7 Dec 2013
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "os/os.h"
+#include "utils/GridConnectHub.hxx"
+#include "executor/Executor.hxx"
+#include "can_frame.h"
+#include "nmranet_config.h"
+
+#include "nmranet/IfCan.hxx"
+#include "nmranet/If.hxx"
+#include "nmranet/AliasAllocator.hxx"
+#include "nmranet/EventService.hxx"
+#include "nmranet/EventHandlerTemplates.hxx"
+#include "nmranet/DefaultNode.hxx"
+#include "utils/socket_listener.hxx"
+
+#include "custom/MemorizingEventHandler.hxx"
+
+
+NO_THREAD nt;
+Executor<1> g_executor(nt);
+Service g_service(&g_executor);
+CanHubFlow can_hub0(&g_service);
+
+//DEFINE_PIPE(can_pipe, &g_executor, sizeof(struct can_frame));
+//DEFINE_PIPE(can_pipe0, &g_executor, sizeof(struct can_frame));
+
+static const nmranet::NodeID NODE_ID = 0x050101011442ULL;
+
+nmranet::IfCan g_if_can(&g_executor, &can_hub0, 3, 3, 2);
+static nmranet::AddAliasAllocator g_alias_allocator(NODE_ID, &g_if_can);
+nmranet::DefaultNode g_node(&g_if_can, NODE_ID);
+nmranet::EventService g_event_service(&g_if_can);
+
+static const uint64_t BRACZ_LAYOUT = 0x0501010114FF0000ULL;
+
+nmranet::MemorizingHandlerManager g_permabits(&g_node, BRACZ_LAYOUT | 0xE000,
+                                              512, 2);
+
+/** Entry point to application.
+ * @param argc number of command line arguments
+ * @param argv array of command line arguments
+ * @return 0, should never return
+ */
+int appl_main(int argc, char* argv[])
+{
+    int conn_fd = ConnectSocket("localhost", 12021);
+    HASSERT(conn_fd >= 0);
+    create_gc_port_for_can_hub(&can_hub0, conn_fd);
+
+    // Bootstraps the alias allocation process.
+    g_if_can.alias_allocator()->send(g_if_can.alias_allocator()->alloc());
+
+    g_executor.thread_body();
+    return 0;
+}
