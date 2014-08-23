e0f1b43da0e5ebe7e7b7cd5a8ec241f03979373b
Balazs Racz <bracz@google.com>
2014-08-23 10:54:15 +0200

    Adds comments declaring which train has which decoder.

--- cs/mobilestation/prod_traindb.cxx
+++ cs/mobilestation/prod_traindb.cxx
@@ -5,10 +5,10 @@ namespace mobilestation {
 
 __attribute__((weak)) extern const struct const_loco_db_t const_lokdb[] = {
   // 0
-  { 50, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0xff} , { LIGHT, ENGINE, HONK, SPEECH, SPEECH, SPEECH, SPEECH, LIGHT1, FNP, ABV, HONK, SOUNDP, SOUNDP, SOUNDP, HONK, HONK, HONK, HONK, HONK, HONK, 0xff }, "ICN", DCC_28 | PUSHPULL },
+  { 50, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0xff} , { LIGHT, ENGINE, HONK, SPEECH, SPEECH, SPEECH, SPEECH, LIGHT1, FNP, ABV, HONK, SOUNDP, SOUNDP, SOUNDP, HONK, HONK, HONK, HONK, HONK, HONK, 0xff }, "ICN", DCC_28 | PUSHPULL },  // ESU loksound V4.0 MM/DCC
   // 1
   { 51, { 0, 1, 3, 4,  0xff, }, { LIGHT, TELEX, FNT11, ABV,  0xff, },
-    "BR 260417", DCC_28 },
+    "BR 260417", DCC_28 },  // ESU LokPilot 3.0
   // 2
   { 12, { 0, 2, 3, 4,  0xff, }, { LIGHT, BEAMER, HONK, ABV,  0xff, },
     "RE 474 014-8", MFX }, // todo: check fnbits
@@ -20,10 +20,10 @@ __attribute__((weak)) extern const struct const_loco_db_t const_lokdb[] = {
     "RE 460 TSR", MARKLIN_NEW }, // todo: there is no beamer here
   // 5
   { 32, { 0, 4,  0xff, }, { LIGHT, ABV,  0xff, },
-    "RTS RAILTR", MARKLIN_NEW },
+    "RTS RAILTR", MARKLIN_NEW },  // Marklin Fx decoder
   // 6
   { 61, { 0, 1, 2, 3, 4,  0xff, }, { LIGHT, ENGINE, LIGHT2, HONK, 7,  0xff, },
-    "MAV M61", MARKLIN_NEW /*DCC_28*/ }, // todo: check fn definition and type mapping
+    "MAV M61", MARKLIN_NEW /*DCC_28*/ }, // todo: check fn definition and type mapping // ESU LokSound V3.5
   // 7
   { 46, { 0, 1, 2, 3, 4, 5, 0xff, }, { LIGHT, HONK, ENGINE, FNT11, ABV, BEAMER,  0xff, },
     "RE 460 118-2", MFX | PUSHPULL },  // todo: there is F5 with beamer that can't be switched.
@@ -32,7 +32,7 @@ __attribute__((weak)) extern const struct const_loco_db_t const_lokdb[] = {
     "RE 4/4 II", DCC_28 /* TODO: this should be dcc 14 */ }, // todo: snail mode
   // 9
   { 47, { 0,  0xff, }, { LIGHT,  0xff, },
-    "RE 465", MARKLIN_OLD | PUSHPULL },
+    "RE 465", MARKLIN_OLD | PUSHPULL },  // Marklin 6090
   // id 10
   { 72, { 0, 1, 4, 0xff, }, { LIGHT, LIGHT1, ABV, 0xff },
     "DHG 700", MARKLIN_NEW },
@@ -47,16 +47,16 @@ __attribute__((weak)) extern const struct const_loco_db_t const_lokdb[] = {
     "WLE ER20", MARKLIN_NEW },
   // id 14
   { 58, { 0, 3, 4,  0xff, }, { LIGHT, FNT11, ABV,  0xff, },
-    "185 595-6", DCC_28 }, // NOTE: hardware is programmed for addr 18
+    "185 595-6", DCC_28 }, // NOTE: hardware is programmed for addr 18 // I think this engine was returned to Acacio
   // id 15
   { 26, { 0,  0xff, }, { LIGHT,  0xff, },
-    "Re 460 HAG", MARKLIN_OLD | PUSHPULL },
+    "Re 460 HAG", MARKLIN_OLD | PUSHPULL },  // Marklin 6090 (i think; best to check)
   // id 16
   { 38, { 0,  0xff, }, { LIGHT,  0xff, },
     "BDe 4/4 1460", MARKLIN_OLD | PUSHPULL },
   // id 17
   { 48, { 0,  0xff, }, { LIGHT,  0xff, },
-    "Re 4/4 II 11239", MARKLIN_NEW },
+    "Re 4/4 II 11239", MARKLIN_NEW }, // ESU pre-lokpilot decoder, no DCC support
   // id 18
   { 66, { 0, 3, 4, 0xff, }, { LIGHT, FNT11, ABV, 0xff, },
     "Re 6/6 11665", DCC_128 },
