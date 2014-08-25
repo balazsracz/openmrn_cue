35c40e3fa84ca4b68b1fb7e54fb55d83bcdf743d
Balazs Racz <balazs.racz@gmail.com>
2014-08-25 11:02:53 +0200

    Allows overriding the send_host_log_event function.

--- cs/custom/HostLogging.cxx
+++ cs/custom/HostLogging.cxx
@@ -5,6 +5,8 @@
 
 namespace bracz_custom {
 
+void send_host_log_event(HostLogEvent e) __attribute__((weak));
+
 void send_host_log_event(HostLogEvent e) {
   PacketBase pkt(2);
   pkt[0] = CMD_CAN_LOG;
