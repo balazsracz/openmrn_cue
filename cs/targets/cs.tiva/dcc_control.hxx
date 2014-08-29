8fc51418cc0cf458bd813f39600d932ede6b3287
Balazs Racz <bracz@google.com>
2014-08-29 09:57:16 +0200

    Adds documentation and query dcc.

--- cs/targets/cs.tiva/dcc_control.hxx
+++ cs/targets/cs.tiva/dcc_control.hxx
@@ -3,8 +3,12 @@
 #define _BRACZ_CUSTOM_DCC_CONTROL_HXX_
 
 extern "C" {
+  /** Turns on DCC track output power. */
 extern void enable_dcc();
+  /** Turns off DCC track output power. */
 extern void disable_dcc();
+  /** @returns whether dcc track power is enabled or not. */
+extern bool query_dcc();
 }
 
 #endif
