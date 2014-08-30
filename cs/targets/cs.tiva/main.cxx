50468410e712f2a221fd189fb05682d991ddc973
Balazs Racz <bracz@google.com>
2014-08-30 20:48:58 +0200

    Updates CS.tiva main: new TivaDCC header and deadband settings. ips local node count. disables host log event on stdout. adds a blinker flow deletes commented-out modules Adds all-tain-from-traindb node enables USBserial protocol

--- cs/targets/cs.tiva/main.cxx
+++ cs/targets/cs.tiva/main.cxx
@@ -75,8 +75,9 @@
 #include "dcc/Loco.hxx"
 #include "mobilestation/TrainDb.hxx"
 #include "dcc/LocalTrackIf.hxx"
+#include "mobilestation/AllTrainNodes.hxx"
 
-#include "TivaDev.hxx"
+#include "TivaDCC.hxx"
 #include "ShortDetection.hxx"
 
 #include "inc/hw_types.h"
@@ -85,8 +86,10 @@
 #include "dcc_control.hxx"
 
 TivaDCC dcc_hw("/dev/mainline", TIMER1_BASE, TIMER0_BASE, INT_TIMER0A,
-               INT_TIMER1A, 16, (56 << 1) * (configCPU_CLOCK_HZ / 1000000),
-               (100 << 1) * (configCPU_CLOCK_HZ / 1000000), 50, 60);
+               INT_TIMER1A, (uint8_t*)(GPIO_PORTQ_BASE + (GPIO_PIN_0 << 2)), 16,
+               500, 500);
+
+#define STANDALONE
 
 extern "C" {
 /** Timer interrupt for DCC packet handling.
@@ -112,10 +115,11 @@ OVERRIDE_CONST(mobile_station_train_count, 0);
 OVERRIDE_CONST(automata_init_backoff, 20000);
 OVERRIDE_CONST(node_init_identify, 0);
 
-OVERRIDE_CONST(dcc_packet_min_refresh_delay_ms, 250);
+OVERRIDE_CONST(dcc_packet_min_refresh_delay_ms, 1);
 
 
 namespace mobilestation {
+/*
 extern const struct const_loco_db_t const_lokdb[];
 
 const struct const_loco_db_t const_lokdb[] = {
@@ -133,6 +137,7 @@ const struct const_loco_db_t const_lokdb[] = {
 };
 extern const size_t const_lokdb_size;
 const size_t const_lokdb_size = sizeof(const_lokdb) / sizeof(const_lokdb[0]);
+*/
 }  // namespace mobilestation
 
 NO_THREAD nt;
@@ -145,22 +150,11 @@ static const nmranet::NodeID NODE_ID = 0x050101011432ULL;
 
 extern "C" {
 extern insn_t automata_code[];
-
-const size_t WRITE_FLOW_THREAD_STACK_SIZE = 900;
-extern const size_t CAN_TX_BUFFER_SIZE;
-extern const size_t CAN_RX_BUFFER_SIZE;
-const size_t CAN_RX_BUFFER_SIZE = 8;
-const size_t CAN_TX_BUFFER_SIZE = 8;
-  extern const size_t SERIAL_RX_BUFFER_SIZE;
-  extern const size_t SERIAL_TX_BUFFER_SIZE;
-const size_t SERIAL_RX_BUFFER_SIZE = 16;
-const size_t SERIAL_TX_BUFFER_SIZE = 16;
-const size_t main_stack_size = 900;
 }
 
 
 HubFlow stdout_hub(&g_service);
-auto* g_gc_adapter = GCAdapterBase::CreateGridConnectAdapter(&stdout_hub, &can_hub0, false);
+//auto* g_gc_adapter = GCAdapterBase::CreateGridConnectAdapter(&stdout_hub, &can_hub0, false);
 
 extern "C" {
 void log_output(char* buf, int size) {
@@ -174,12 +168,14 @@ void log_output(char* buf, int size) {
 }
 
 namespace bracz_custom {
-void send_host_log_event(HostLogEvent e) {
+void xx_send_host_log_event(HostLogEvent e) {
   log_output((char*)&e, 1);
 }
 }
 
-nmranet::IfCan g_if_can(&g_executor, &can_hub0, 3, 3, 3);
+static const int kLocalNodesCount = 30;
+nmranet::IfCan g_if_can(&g_executor, &can_hub0, kLocalNodesCount, 3,
+                        kLocalNodesCount);
 static nmranet::AddAliasAllocator _alias_allocator(NODE_ID, &g_if_can);
 nmranet::DefaultNode g_node(&g_if_can, NODE_ID);
 nmranet::EventService g_event_service(&g_if_can);
@@ -188,6 +184,43 @@ static const uint64_t EVENT_ID = 0x0501010114FF203AULL;
 const int main_priority = 0;
 
 extern "C" { void resetblink(uint32_t pattern); }
+
+class BlinkerFlow : public StateFlowBase
+{
+public:
+    BlinkerFlow(nmranet::Node* node)
+        : StateFlowBase(node->interface()),
+          state_(1),
+          bit_(node, EVENT_ID, EVENT_ID + 1, &state_, (uint8_t)1),
+          producer_(&bit_),
+          sleepData_(this)
+    {
+        start_flow(STATE(blinker));
+    }
+
+private:
+    Action blinker()
+    {
+        state_ = !state_;
+#ifdef __linux__
+        LOG(INFO, "blink produce %d", state_);
+#endif
+        producer_.Update(&helper_, n_.reset(this));
+        return wait_and_call(STATE(handle_sleep));
+    }
+
+    Action handle_sleep()
+    {
+        return sleep_and_call(&sleepData_, MSEC_TO_NSEC(1000), STATE(blinker));
+    }
+
+    uint8_t state_;
+    nmranet::MemoryBit<uint8_t> bit_;
+    nmranet::BitEventProducer producer_;
+    nmranet::WriteHelper helper_;
+    StateFlowTimer sleepData_;
+    BarrierNotifiable n_;
+};
 
 class LoggingBit : public nmranet::BitEventInterface
 {
@@ -223,28 +256,26 @@ private:
 
 
 dcc::LocalTrackIf track_if(&g_service, 2);
-//bracz_custom::TrackIfSend track_send(&can_hub1);
 commandstation::UpdateProcessor cs_loop(&g_service, &track_if);
 commandstation::PoolToQueueFlow<Buffer<dcc::Packet>> pool_translator(&g_service, track_if.pool(), &cs_loop);
-
-//bracz_custom::TrackIfReceive track_recv(&can1_interface, &cs_loop);
-static const uint64_t ON_EVENT_ID = 0x0501010114FF0004ULL;
-//bracz_custom::TrackPowerOnOffBit on_off(ON_EVENT_ID, ON_EVENT_ID+1, &track_send);
-//nmranet::BitEventConsumer powerbit(&on_off);
+TivaTrackPowerOnOffBit on_off(nmranet::TractionDefs::CLEAR_EMERGENCY_STOP_EVENT,
+                              nmranet::TractionDefs::EMERGENCY_STOP_EVENT);
+nmranet::BitEventConsumer powerbit(&on_off);
 nmranet::TrainService traction_service(&g_if_can);
 
-
-dcc::Dcc28Train train_Am843(dcc::DccShortAddress(43));
-nmranet::TrainNode train_Am843_node(&traction_service, &train_Am843);
-dcc::Dcc28Train train_Re460(dcc::DccShortAddress(22));
-//dcc::MMNewTrain train_Re460(dcc::MMAddress(22));
+//dcc::Dcc28Train train_Am843(dcc::DccShortAddress(43));
+//nmranet::TrainNode train_Am843_node(&traction_service, &train_Am843);
+//dcc::Dcc28Train train_Re460(dcc::DccShortAddress(22));
+dcc::MMNewTrain train_Re460(dcc::MMAddress(22));
 nmranet::TrainNode train_Re460_node(&traction_service, &train_Re460);
 
 
 //mobilestation::MobileStationSlave mosta_slave(&g_executor, &can1_interface);
 mobilestation::TrainDb train_db;
-//CanIf can1_interface(&g_service, &can_hub1);
-//mobilestation::MobileStationTraction mosta_traction(&can1_interface, &g_if_can, &train_db, &g_node);
+CanIf can1_interface(&g_service, &can_hub1);
+mobilestation::MobileStationTraction mosta_traction(&can1_interface, &g_if_can, &train_db, &g_node);
+
+//mobilestation::AllTrainNodes all_trains(&train_db, &traction_service);
 
 
 void mydisable()
@@ -255,6 +286,8 @@ void mydisable()
 }
 
 TivaShortDetectionModule g_short_det(&g_service, MSEC_TO_NSEC(1));
+BlinkerFlow blinker(&g_node);
+
 
 /** Entry point to application.
  * @param argc number of command line arguments
@@ -266,10 +299,14 @@ int appl_main(int argc, char* argv[])
   //  mydisable();
     start_watchdog(5000);
     add_watchdog_reset_timer(500);
-    //PacketQueue::initialize("/dev/serUSB0");
+#ifdef STANDALONE
+    PacketQueue::initialize("/dev/serUSB0");
+#else
+    PacketQueue::initialize("/dev/serUSB0");
+#endif
     //HubDeviceNonBlock<CanHubFlow> can0_port(&can_hub0, "/dev/can0");
     //HubDeviceNonBlock<CanHubFlow> can1_port(&can_hub1, "/dev/can1");
-    //bracz_custom::init_host_packet_can_bridge(&can_hub1);
+    bracz_custom::init_host_packet_can_bridge(&can_hub1);
     FdHubPort<HubFlow> stdout_port(&stdout_hub, 0, EmptyNotifiable::DefaultInstance());
 
     int mainline = open("/dev/mainline", O_RDWR);
@@ -279,9 +316,9 @@ int appl_main(int argc, char* argv[])
     nmranet::Velocity v;
     v.set_mph(29);
     //XXtrain_Re460.set_speed(v);
-    train_Am843.set_speed(v);
+    /*train_Am843.set_speed(v);
     train_Am843.set_fn(0, 1);
-    train_Re460.set_fn(0, 1);
+    train_Re460.set_fn(0, 1);*/
 
     /*int fd = open("/dev/can0", O_RDWR);
     ASSERT(fd >= 0);
@@ -298,32 +335,14 @@ int appl_main(int argc, char* argv[])
     //AutomataRunner runner(&g_node, automata_code);
     //resume_all_automata();
 
-    // Starts dcc output.
+#ifdef STANDALONE
+    // Start dcc output
     enable_dcc();
+#else
+    // Do not start dcc output.
+    disable_dcc();
+#endif
 
     g_executor.thread_body();
     return 0;
-
-    /*
-    startpin.mode(PullUp);
-    PacketQueue::initialize("/dev/serUSB0");
-    can_pipe0.AddPhysicalDeviceToPipe("/dev/can1", "can0_rx_thread", 512);
-    can_pipe1.AddPhysicalDeviceToPipe("/dev/can0", "can1_rx_thread", 512);
-#ifdef TARGET_LPC11Cxx
-    lpc11cxx::CreateCanDriver(&can_pipe);
-#endif
-    LoggingBit logger(EVENT_ID, EVENT_ID + 1, "blinker");
-    nmranet::BitEventConsumer consumer(&logger);
-    BlinkerFlow blinker(&g_node);
-    g_if_can.AddWriteFlows(1, 1);
-    g_if_can.set_alias_allocator(
-        new nmranet::AsyncAliasAllocator(NODE_ID, &g_if_can));
-    nmranet::AliasInfo info;
-    g_if_can.alias_allocator()->empty_aliases()->Release(&info);
-    nmranet::AddEventHandlerToIf(&g_if_can);
-    resetblink(0x80002A);
-    while(startpin) {}
-    resetblink(0);
-    g_executor.ThreadBody();
-    return 0;*/
 }
