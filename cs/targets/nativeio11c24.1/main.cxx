26b665ee1090740e0f561cfb818c930765b85be1
Balazs Racz <bracz@google.com>
2014-09-29 18:48:35 +0200

    Rewrites allgpioproducers to use a stateflow instead of hand-written freertos timers.

--- cs/targets/nativeio11c24.1/main.cxx
+++ cs/targets/nativeio11c24.1/main.cxx
@@ -101,7 +101,95 @@ MbedGPIOListener listen_r1(R_EVENT_ID + 2, R_EVENT_ID + 3, P0_2);
 MbedGPIOListener listen_r2(R_EVENT_ID + 4, R_EVENT_ID + 5, P2_7);
 MbedGPIOListener listen_r3(R_EVENT_ID + 6, R_EVENT_ID + 7, P2_8);
 
-class AllGPIOProducers : private Executable, private Atomic {
+
+class AllGPIOProducers : public StateFlowBase {
+ public:
+  AllGPIOProducers(uint64_t event_base)
+      : StateFlowBase(&g_service),
+        exported_bits_(0),
+        producer_(&g_node, event_base, &exported_bits_, kPinCount),
+        timer_(this) {
+    int next = 0;
+    AddPort(next++, P2_11);
+    AddPort(next++, P0_6);  // TODO: this should  be 0_10 but that's SWCLK
+    AddPort(next++, P0_9);
+    AddPort(next++, P0_8);
+    HASSERT(next == kPinCount);
+    InitCounts();
+    start_flow(STATE(sleep_for_poll));
+  }
+
+ private:
+  void AddPort(int num, PinName pin) {
+    mask_[num] = gpio_set(pin);
+    gpio_t gpio;
+    gpio_init(&gpio, pin, PIN_INPUT);
+    gpio_mode(&gpio, PullNone);
+    memory_[num] = gpio.reg_in;
+  }
+
+  void InitCounts() {
+    memset(counts_, 0, sizeof(counts_));
+    max_count_ = 0;
+  }
+
+  Action sleep_for_poll() {
+    return sleep_and_call(&timer_, MSEC_TO_NSEC(1), STATE(poll));
+  }
+
+  Action poll() {
+    for (int i = 0; i < kPinCount; ++i) {
+      if ((*memory_[i]) & mask_[i]) {
+        counts_[i]++;
+      }
+    }
+    if (++max_count_ == 0xff) {
+      // Evaluates the captured data.
+      last_read_bits_ = 0;
+      for (int i = 0; i < kPinCount; ++i) {
+        if (counts_[i] > 128) {
+          last_read_bits_ |= (1<<i);
+        }
+      }
+      InitCounts();
+      // Schedule callback on the main thread to send off events.
+      return call_immediately(STATE(send_off_events));
+    }
+    return call_immediately(STATE(sleep_for_poll));
+  }
+
+  Action send_off_events() {
+    for (int i = 0; i < kPinCount; ++i) {
+      if (!helper_busy_.is_done()) return wait();
+      uint32_t mask =  1<<i;
+      bool new_value = (last_read_bits_ & mask) ? true : false;
+      producer_.Set(i, new_value, &helper_, helper_busy_.reset(this));
+    }
+    return call_immediately(STATE(sleep_for_poll));
+  }
+
+  static const int kPinCount = 4;
+
+  volatile uint32_t* memory_[kPinCount];
+  uint32_t mask_[kPinCount];
+
+  uint8_t counts_[kPinCount];
+  uint8_t max_count_;
+  uint8_t pending_ {0};
+
+  // Backing store of the range producer.
+  uint32_t exported_bits_;
+  // Fresh bits that we need to copy to the exported bits.
+  uint32_t last_read_bits_;
+
+  nmranet::BitRangeEventPC producer_;
+
+  nmranet::WriteHelper helper_;
+  BarrierNotifiable helper_busy_;
+  StateFlowTimer timer_;
+};
+
+/*class AllGPIOProducers : private Executable, private Atomic {
  public:
   AllGPIOProducers(uint64_t event_base)
       : exported_bits_(0),
@@ -156,7 +244,7 @@ class AllGPIOProducers : private Executable, private Atomic {
     max_count_ = 0;
   }
 
-  static long long timer_callback(void* object, void* /*unused*/) {
+  static long long timer_callback(void* object, void* unused) {
     AllGPIOProducers* me = static_cast<AllGPIOProducers*>(object);
     for (int i = 0; i < kPinCount; ++i) {
       if ((*me->memory_[i]) & me->mask_[i]) {
@@ -199,7 +287,7 @@ class AllGPIOProducers : private Executable, private Atomic {
 
   nmranet::WriteHelper helper_;
   BarrierNotifiable helper_busy_;
-};
+  };*/
 
 static const uint64_t DET_EVENT_ID = 0x0501010114FF2810ULL;
 AllGPIOProducers input_producers_(DET_EVENT_ID);
@@ -217,7 +305,7 @@ int appl_main(int argc, char* argv[])
   //BlinkerFlow blinker(&g_node);
     LoggingBit logger(EVENT_ID, EVENT_ID + 1, "blinker");
     nmranet::BitEventConsumer consumer(&logger);
-    g_if_can.add_addressed_message_support();
+    //g_if_can.add_addressed_message_support();
     nmranet::AliasInfo info;
     // Bootstraps the alias allocation process.
     g_if_can.alias_allocator()->send(g_if_can.alias_allocator()->alloc());
