11d9b3e56be7441a73f7eb46c39e5480f0679af6
Balazs Racz <bracz@google.com>
2014-12-13 12:17:36 +0100

    Updates deadband delay for the better hardware of the CS tiva.

--- cs/targets/cs.tiva/DccHardware.hxx
+++ cs/targets/cs.tiva/DccHardware.hxx
@@ -105,10 +105,10 @@ struct DccHwDefs {
 
   /** the time (in nanoseconds) to wait between turning off the low driver and
    * turning on the high driver. */
-  static const int H_DEADBAND_DELAY_NSEC = 6000;
+  static const int H_DEADBAND_DELAY_NSEC = 2000;
   /** the time (in nanoseconds) to wait between turning off the high driver and
    * turning on the low driver. */
-  static const int L_DEADBAND_DELAY_NSEC = 6000;
+  static const int L_DEADBAND_DELAY_NSEC = 2000;
 
   /** @returns true to produce the RailCom cutout, else false */
   static bool railcom_cutout() { return true; }
