fbef64b31fc59ac93a60b56b35d41a0cc8d1c5b8
bracz <bracz@localhost>
2015-02-01 01:40:03 -0600

    Adds a simple blinker logic project.

---
+++ automata/stbaker-logic.cc
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <string>
+#include <fstream>
+#include <memory>
+using namespace std;
+
+#include "system.hxx"
+#include "operations.hxx"
+#include "variables.hxx"
+#include "registry.hxx"
+
+#include "bracz-layout.hxx"
+#include "control-logic.hxx"
+
+//#define OFS_GLOBAL_BITS 24
+
+using namespace automata;
+
+StateRef StUser1(10);
+StateRef StUser2(11);
+
+Board brd;
+
+EventBasedVariable is_paused(&brd, "is_paused", BRACZ_LAYOUT | 0x0000,
+                             BRACZ_LAYOUT | 0x0001, 7, 31, 3);
+
+EventBasedVariable blink_off(&brd, "blink_off", BRACZ_LAYOUT | 0x0002,
+                             BRACZ_LAYOUT | 0x0003, 7, 31, 2);
+
+EventBasedVariable power_acc(&brd, "power_accessories", BRACZ_LAYOUT | 0x0004,
+                             BRACZ_LAYOUT | 0x0005, 7, 31, 1);
+
+EventBasedVariable short_det(&brd, "short_detected", BRACZ_LAYOUT | 0x0006,
+                             BRACZ_LAYOUT | 0x0007, 7, 31, 0);
+
+EventBasedVariable overcur(&brd, "overcurrent_detected", BRACZ_LAYOUT | 0x0008,
+                           BRACZ_LAYOUT | 0x0009, 7, 30, 7);
+
+EventBasedVariable sendmeasure(&brd, "send_current_measurements",
+                               BRACZ_LAYOUT | 0x000a, BRACZ_LAYOUT | 0x000b, 7,
+                               30, 6);
+
+EventBasedVariable watchdog(&brd, "reset_watchdog",
+                            BRACZ_LAYOUT | 0x0010, BRACZ_LAYOUT | 0x0010, 7,
+                            30, 5);
+
+EventBasedVariable reset_all_routes(&brd, "reset_routes",
+                            BRACZ_LAYOUT | 0x0012, BRACZ_LAYOUT | 0x0013, 7,
+                            30, 4);
+
+
+AccBoard bd(0x2d);
+
+EventBlock logic(&brd, BRACZ_LAYOUT | 0xE000, "logic");
+
+int next_temp_bit = 480;
+GlobalVariable* NewTempVariable(Board* board) {
+  int counter = next_temp_bit++;
+  int client, offset, bit;
+  if (!DecodeOffset(counter, &client, &offset, &bit)) {
+    fprintf(stderr, "Too many local variables. Cannot allocate more.");
+    abort();
+  }
+  return new EventBasedVariable(board, StringPrintf("tmp_var_%d", counter),
+                                BRACZ_LAYOUT | 0x3000 | (counter << 1),
+                                BRACZ_LAYOUT | 0x3000 | (counter << 1) | 1,
+                                client, offset, bit);
+}
+
+unique_ptr<GlobalVariable> blink_variable(NewTempVariable(&brd));
+
+EventBasedVariable led(&brd, "led", 0x0502010202650012ULL,
+                       0x0502010202650013ULL, 7, 31, 1);
+
+DefAut(watchdog, brd, {
+    LocalVariable* w = ImportVariable(&watchdog);
+    Def().IfTimerDone().ActReg1(w).ActReg0(w).ActTimer(1);
+  });
+
+DefAut(blinkaut, brd, {
+  const int kBlinkSpeed = 3;
+  LocalVariable* rep(ImportVariable(blink_variable.get()));
+  const LocalVariable& lblink_off(ImportVariable(blink_off));
+  Def().IfState(StInit).ActState(StUser1);
+  Def()
+      .IfState(StUser1)
+      .IfTimerDone()
+      .IfReg0(lblink_off)
+      .ActTimer(kBlinkSpeed)
+      .ActState(StUser2)
+      .ActReg0(rep);
+  Def()
+      .IfState(StUser2)
+      .IfTimerDone()
+      .IfReg0(lblink_off)
+      .ActTimer(kBlinkSpeed)
+      .ActState(StUser1)
+      .ActReg1(rep);
+
+  DefCopy(*rep, ImportVariable(&bd.LedGoldSw));
+});
+
+int main(int argc, char** argv) {
+  automata::reset_routes = &reset_all_routes;
+
+  FILE* f = fopen("automata.bin", "wb");
+  assert(f);
+  string output;
+  brd.Render(&output);
+  fwrite(output.data(), 1, output.size(), f);
+  fclose(f);
+
+  //  f = fopen("convention-logic.cout", "wb");
+  f = stdout;
+  fprintf(f, "const char automata_code[] = {\n  ");
+  int c = 0;
+  for (char t : output) {
+    fprintf(f, "0x%02x, ", (uint8_t)t);
+    if (++c >= 12) {
+      fprintf(f, "\n  ");
+      c = 0;
+    }
+  }
+  fprintf(f, "};\n");
+  fclose(f);
+
+  /*if (automata::GetOffsetMap()) {
+    f = fopen("variables.txt", "w");
+    assert(f);
+    map<int, string>& m(*automata::GetOffsetMap());
+    for (const auto& it : m) {
+      fprintf(f, "%04x: %s\n", it.first * 2, it.second.c_str());
+    }
+    fclose(f);
+    }*/
+
+  f = fopen("jmri-out.xml", "w");
+  assert(f);
+  PrintAllEventVariables(f);
+  fclose(f);
+
+  f = fopen("var-bash-list.txt", "w");
+  assert(f);
+  PrintAllEventVariablesInBashFormat(f);
+  fclose(f);
+
+
+
+  return 0;
+};
