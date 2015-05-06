ca121de5b3e49e48b4841fb64e87d52a514c8b04
bracz <bracz@localhost>
2015-05-06 02:25:50 -0700

    Factors out the rpc test helper to a separate file.

---
+++ cs/server/rpc_test_helper.hxx
@@ -0,0 +1,81 @@
+#ifndef _SERVER_RPC_TEST_HELPER_HXX_
+#define _SERVER_RPC_TEST_HELPER_HXX_
+
+#include "google/protobuf/text_format.h"
+#include "gmock/gmock.h"
+
+using ::testing::StrictMock;
+
+namespace server {
+namespace {
+
+class MockResponsePacket : public PacketFlowInterface {
+ public:
+  MOCK_METHOD1(received_packet, void(const string&));
+
+  void send(Buffer<string>* b, unsigned priority) override {
+    TinyRpcResponse resp;
+    resp.ParseFromString(*b->data());
+    string text;
+    HASSERT(::google::protobuf::TextFormat::PrintToString(resp, &text));
+    received_packet(text);
+    b->unref();
+  }
+};
+
+string CanonicalizeProto(const string& text_pb) {
+  TinyRpcResponse resp;
+  HASSERT(::google::protobuf::TextFormat::ParseFromString(text_pb, &resp));
+  string canon;
+  HASSERT(::google::protobuf::TextFormat::PrintToString(resp, &canon));
+  return canon;
+}
+
+class RpcServiceTestHelper {
+protected:
+  RpcServiceTestHelper(RpcService* s) : s_(s) {
+    ::pipe(down_pipe_);
+    ::pipe(up_pipe_);
+
+    send_.reset(new PacketStreamSender(s_, down_pipe_[1]));
+    recv_.reset(new PacketStreamReceiver(s_, &response_handler_, up_pipe_[0]));
+    s_->set_channel(down_pipe_[0], up_pipe_[1]);
+  }
+
+  ~RpcServiceTestHelper() {
+    wait();
+    ::close(down_pipe_[0]);
+    ::close(down_pipe_[1]);
+    ::close(up_pipe_[0]);
+    ::close(up_pipe_[1]);
+  }
+
+  void wait() {
+    do {
+      usleep(1000);
+      wait_for_main_executor();
+    } while (!send_->is_waiting() || s_->is_busy());
+  }
+
+  void send_request(const TinyRpcRequest& r) {
+    auto* b = send_->alloc();
+    r.SerializeToString(b->data());
+    send_->send(b);
+  }
+  
+protected:
+  StrictMock<MockResponsePacket> response_handler_;
+
+private:
+  RpcService* s_;
+  int down_pipe_[2];
+  int up_pipe_[2];
+  std::unique_ptr<PacketStreamSender> send_;
+  std::unique_ptr<PacketStreamReceiver> recv_;
+};
+
+
+}  // namespace
+}  // namespace server
+
+#endif // _SERVER_RPC_TEST_HELPER_HXX_
