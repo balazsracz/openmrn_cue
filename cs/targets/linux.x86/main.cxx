1f633be847e332f8bb6dfe3824e06e40bd5e3562
bracz <bracz@localhost>
2015-05-08 17:55:49 +0200

    Adds a couple of fake trains to the cs.linux app. Adds the mobilestation traction translator and the hostclient.

--- cs/targets/linux.x86/main.cxx
+++ cs/targets/linux.x86/main.cxx
@@ -35,33 +35,68 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "nmranet/SimpleStack.hxx"
 #include "commandstation/UpdateProcessor.hxx"
-#include "nmranet/TractionTestTrain.hxx"
 #include "mobilestation/MobileStationTraction.hxx"
 #include "mobilestation/TrainDb.hxx"
 #include "nmranet/SimpleNodeInfoMockUserFile.hxx"
+#include "nmranet/SimpleStack.hxx"
+#include "nmranet/TractionTestTrain.hxx"
 #include "nmranet/TractionTrain.hxx"
+#include "custom/HostProtocol.hxx"
 
 static const nmranet::NodeID NODE_ID = 0x050101011440ULL;
 nmranet::SimpleCanStack stack(NODE_ID);
 OVERRIDE_CONST(num_memory_spaces, 4);
+OVERRIDE_CONST(local_nodes_count, 4);
+// Forces all trains to be our responsibility.
+OVERRIDE_CONST(mobile_station_train_count, 0);
 
 nmranet::MockSNIPUserFile snip_user_file("Default user name",
                                          "Default user description");
-const char *const nmranet::SNIP_DYNAMIC_FILENAME = nmranet::MockSNIPUserFile::snip_user_file_path;
+const char* const nmranet::SNIP_DYNAMIC_FILENAME =
+    nmranet::MockSNIPUserFile::snip_user_file_path;
 
+nmranet::TrainService traction_service(stack.interface());
 
+nmranet::LoggingTrain train_a(43);
+nmranet::LoggingTrain train_b(22);
+nmranet::LoggingTrain train_c(465);
+nmranet::TrainNode node_a(&traction_service, &train_a);
+nmranet::TrainNode node_b(&traction_service, &train_b);
+nmranet::TrainNode node_c(&traction_service, &train_c);
+
+CanHubFlow can_hub1(stack.service());
+CanIf can_if1(stack.service(), &can_hub1);
+
+mobilestation::TrainDb train_db;
+mobilestation::MobileStationTraction mosta_traction(&can_if1, stack.interface(),
+                                                    &train_db, stack.node());
+
+namespace mobilestation {
+const struct const_loco_db_t const_lokdb[] = {
+    // 0
+  { 43, { 0, 1, 3, 4,  0xff, }, { LIGHT, TELEX, FNT11, ABV,  0xff, },
+    "Am 843 093-6", DCC_28 },
+  { 22, { 0, 3, 4,  0xff, }, { LIGHT, FNT11, ABV,  0xff, },
+    "RE 460 TSR", DCC_128 }, // todo: there is no beamer here // LD-32 decoder
+  { 465, { 0, 1, 0xff, }, { LIGHT, SPEECH,  0xff, },
+    "Jim's steam", DCC_28 | PUSHPULL },
+  {0, {0, }, {0, }, "", 0}, };
+
+extern const size_t const_lokdb_size =
+    sizeof(const_lokdb) / sizeof(const_lokdb[0]);
+}
+
+bracz_custom::HostClient host_client(stack.dg_service(), stack.node(), &can_hub1);
 
 /** Entry point to application.
  * @param argc number of command line arguments
  * @param argv array of command line arguments
  * @return 0, should never return
  */
-int appl_main(int argc, char* argv[])
-{
-    stack.connect_tcp_gridconnect_hub("localhost", 12021);
+int appl_main(int argc, char* argv[]) {
+  stack.connect_tcp_gridconnect_hub("localhost", 12021);
 
-    stack.loop_executor();
-    return 0;
+  stack.loop_executor();
+  return 0;
 }
