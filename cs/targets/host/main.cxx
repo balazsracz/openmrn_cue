353f8f7f9d3708c380c787ec63439e51de81f5ec
bracz <bracz@localhost>
2015-05-07 20:17:20 -0700

    Adds option handling ans basic initialization code to the host server.

--- cs/targets/host/main.cxx
+++ cs/targets/host/main.cxx
@@ -51,59 +51,105 @@ int port = 12021;
 const char *host = "localhost";
 const char *device_path = nullptr;
 
+int proxy_port = 50001;
+const char *proxy_host = "28k.ch";
+uint64_t destination_nodeid = 0;
+uint16_t destination_alias = 0;
+const char *lokdb_path;
+string lokdb = "LokDb { }";
+
 nmranet::SimpleCanStack stack(NODE_ID);
 
 nmranet::MockSNIPUserFile snip_user_file("Default user name",
                                          "Default user description");
-const char *const nmranet::SNIP_DYNAMIC_FILENAME = nmranet::MockSNIPUserFile::snip_user_file_path;
+const char *const nmranet::SNIP_DYNAMIC_FILENAME =
+    nmranet::MockSNIPUserFile::snip_user_file_path;
 
 namespace nmranet {
 const SimpleNodeStaticValues SNIP_STATIC_DATA = {
     4, "OpenMRN", "Host server", "No hardware here", "0.92"};
+}  // namespace nmranet
+
+server::TrainControlService control_server(stack.executor());
+
+void usage(const char *e) {
+  fprintf(stderr, "Usage: %s", e);
+  fprintf(stderr, R"usage(
+ [-i destination_host] [-p port] [-d device_path] 
+            (-n nodeid | -a alias) [-R proxy_host] [-P proxy_port]
+
+Host server for android train connections.
+
+The bus connection will be through an OpenLCB HUB on destination_host:port with
+OpenLCB over TCP (in GridConnect format) protocol, and/or through the CAN-USB
+device (also in GridConnect protocol) found at device_path. If both are
+specified, then the two connections will be bridged.
+
+        -i destination_host -p port specifies the connection to GridConnect CAN
+           hub over TCP. This defaults to localhost:12021. Specify port=-1 to
+           disable.
+
+        -n 0x0501010114FF or -a 0x123 defines the node ID or the alias to reach
+           the command station through.
+
+        -R host -P port specifies the location of the proxy server for the
+           android connections.
+)usage");
+  exit(1);
 }
 
-void usage(const char *e)
-{
-    fprintf(stderr,
-            "Usage: %s ([-i destination_host] [-p port] | [-d device_path]) "
-            "           \n",
-            e);
-    fprintf(stderr,
-            "Connects to an openlcb bus and exports a virtual train.\n");
-    fprintf(stderr,
-            "The bus connection will be through an OpenLCB HUB on "
-            "destination_host:port with OpenLCB over TCP "
-            "(in GridConnect format) protocol, or through the CAN-USB device "
-            "(also in GridConnect protocol) found at device_path. Device takes "
-            "precedence over TCP host:port specification.");
-    fprintf(stderr, "The default target is localhost:12021.\n");
-    exit(1);
-}
-
-void parse_args(int argc, char *argv[])
-{
-    int opt;
-    while ((opt = getopt(argc, argv, "hp:i:d:a:")) >= 0)
-    {
-        switch (opt)
-        {
-            case 'h':
-                usage(argv[0]);
-                break;
-            case 'p':
-                port = atoi(optarg);
-                break;
-            case 'i':
-                host = optarg;
-                break;
-            case 'd':
-                device_path = optarg;
-                break;
-            default:
-                fprintf(stderr, "Unknown option %c\n", opt);
-                usage(argv[0]);
-        }
+void parse_args(int argc, char *argv[]) {
+  int opt;
+  while ((opt = getopt(argc, argv, "hp:i:d:n:a:R:P:l:")) >= 0) {
+    switch (opt) {
+      case 'h':
+        usage(argv[0]);
+        break;
+      case 'p':
+        port = atoi(optarg);
+        break;
+      case 'i':
+        host = optarg;
+        break;
+      case 'P':
+        proxy_port = atoi(optarg);
+        break;
+      case 'R':
+        proxy_host = optarg;
+        break;
+      case 'd':
+        device_path = optarg;
+        break;
+      case 'l':
+        lokdb_path = optarg;
+        break;
+      case 'n':
+        destination_nodeid = strtoll(optarg, nullptr, 16);
+        break;
+      case 'a':
+        destination_alias = strtoul(optarg, nullptr, 16);
+        break;
+      default:
+        fprintf(stderr, "Unknown option %c\n", opt);
+        usage(argv[0]);
     }
+  }
+}
+
+void load_lokdb(const char *filename) {
+  int fd = ::open(filename, O_RDONLY);
+  if (fd < 0) {
+    LOG(FATAL, "Failed to open lokdb file %s.", filename);
+    return;
+  }
+  string data;
+  char buf[1000];
+  do {
+    int rd = ::read(fd, buf, sizeof(buf));
+    if (rd <= 0) break;
+    data.append(buf, rd);
+  } while (true);
+  lokdb = data;
 }
 
 /** Entry point to application.
@@ -111,15 +157,29 @@ void parse_args(int argc, char *argv[])
  * @param argv array of command line arguments
  * @return 0, should never return
  */
-int appl_main(int argc, char* argv[])
-{
-    stack.print_all_packets();
+int appl_main(int argc, char *argv[]) {
+  parse_args(argc, argv);
+  if (lokdb_path) {
+    LOG(INFO, "Loading lokdb from %s.", lokdb_path);
+    load_lokdb(lokdb_path);
+  }
+  stack.print_all_packets();
+  if (port >= 0) {
     stack.connect_tcp_gridconnect_hub(host, port);
-    if (device_path) {
-      stack.add_gridconnect_port(device_path);
-    }
-    stack.start_tcp_hub_server(12021);
+  }
+  if (device_path) {
+    stack.add_gridconnect_port(device_path);
+  }
+  int proxy_fd = ConnectSocket(proxy_host, proxy_port);
+  if (proxy_fd < 0) {
+    DIE("Could not connect to proxy.");
+  }
+  control_server.initialize(
+      stack.dg_service(), stack.node(),
+      nmranet::NodeHandle(destination_nodeid, destination_alias), lokdb);
+  control_server.set_channel(proxy_fd, proxy_fd);
+  LOG(INFO, "Stack initalized.");
 
-    stack.loop_executor();
-    return 0;
+  stack.loop_executor();
+  return 0;
 }
