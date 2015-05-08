f6d7ffd4030613683fb2caa131c4c2fda6e1d672
bracz <bracz@localhost>
2015-05-07 20:17:05 -0700

    strips down linux CS main code to a simple stack only.

--- cs/targets/linux.x86/main.cxx
+++ cs/targets/linux.x86/main.cxx
@@ -35,116 +35,23 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "os/os.h"
-#include "utils/GridConnectHub.hxx"
-#include "executor/Executor.hxx"
-#include "can_frame.h"
-#include "nmranet_config.h"
-
-#include "nmranet/IfCan.hxx"
-#include "nmranet/If.hxx"
-#include "nmranet/AliasAllocator.hxx"
-#include "nmranet/EventService.hxx"
-#include "nmranet/EventHandlerTemplates.hxx"
-#include "nmranet/DefaultNode.hxx"
-#include "utils/socket_listener.hxx"
-
-#include "src/automata_runner.h"
-#include "src/automata_control.h"
-
-NO_THREAD nt;
-Executor<1> g_executor(nt);
-Service g_service(&g_executor);
-CanHubFlow can_hub0(&g_service);
-
-//DEFINE_PIPE(can_pipe, &g_executor, sizeof(struct can_frame));
-//DEFINE_PIPE(can_pipe0, &g_executor, sizeof(struct can_frame));
+#include "nmranet/SimpleStack.hxx"
+#include "commandstation/UpdateProcessor.hxx"
+#include "nmranet/TractionTestTrain.hxx"
+#include "mobilestation/MobileStationTraction.hxx"
+#include "mobilestation/TrainDb.hxx"
+#include "nmranet/SimpleNodeInfoMockUserFile.hxx"
+#include "nmranet/TractionTrain.hxx"
 
 static const nmranet::NodeID NODE_ID = 0x050101011440ULL;
+nmranet::SimpleCanStack stack(NODE_ID);
+OVERRIDE_CONST(num_memory_spaces, 4);
 
-extern "C" {
-extern insn_t automata_code[];
-}
-
-nmranet::IfCan g_if_can(&g_executor, &can_hub0, 3, 3, 2);
-static nmranet::AddAliasAllocator _alias_allocator(NODE_ID, &g_if_can);
-nmranet::DefaultNode g_node(&g_if_can, NODE_ID);
-nmranet::EventService g_event_service(&g_if_can);
-
-static const uint64_t EVENT_ID = 0x050101011441FF00ULL;
+nmranet::MockSNIPUserFile snip_user_file("Default user name",
+                                         "Default user description");
+const char *const nmranet::SNIP_DYNAMIC_FILENAME = nmranet::MockSNIPUserFile::snip_user_file_path;
 
 
-class BlinkerFlow : public StateFlowBase
-{
-public:
-    BlinkerFlow(nmranet::Node* node)
-        : StateFlowBase(node->interface()),
-          state_(1),
-          bit_(node, EVENT_ID, EVENT_ID + 1, &state_, (uint8_t)1),
-          producer_(&bit_),
-          sleepData_(this)
-    {
-        start_flow(STATE(blinker));
-    }
-
-private:
-    Action blinker()
-    {
-        state_ = !state_;
-#ifdef __linux__
-        LOG(INFO, "blink produce %d", state_);
-#endif
-        producer_.Update(&helper_, n_.reset(this));
-        return wait_and_call(STATE(handle_sleep));
-    }
-
-    Action handle_sleep()
-    {
-        return sleep_and_call(&sleepData_, MSEC_TO_NSEC(1000), STATE(blinker));
-    }
-
-    uint8_t state_;
-    nmranet::MemoryBit<uint8_t> bit_;
-    nmranet::BitEventProducer producer_;
-    nmranet::WriteHelper helper_;
-    StateFlowTimer sleepData_;
-    BarrierNotifiable n_;
-};
-
-extern "C" { void resetblink(uint32_t pattern); }
-
-class LoggingBit : public nmranet::BitEventInterface
-{
-public:
-    LoggingBit(uint64_t event_on, uint64_t event_off, const char* name)
-        : BitEventInterface(event_on, event_off), name_(name), state_(false)
-    {
-    }
-
-    virtual bool GetCurrentState()
-    {
-        return state_;
-    }
-    virtual void SetState(bool new_value)
-    {
-        state_ = new_value;
-        //HASSERT(0);
-#ifdef __linux__
-        LOG(INFO, "bit %s set to %d", name_, state_);
-#else
-        resetblink(state_ ? 1 : 0);
-#endif
-    }
-
-    virtual nmranet::Node* node()
-    {
-        return &g_node;
-    }
-
-private:
-    const char* name_;
-    bool state_;
-};
 
 /** Entry point to application.
  * @param argc number of command line arguments
@@ -153,20 +60,8 @@ private:
  */
 int appl_main(int argc, char* argv[])
 {
-    int conn_fd = ConnectSocket("localhost", 8082);
-    HASSERT(conn_fd >= 0);
-    create_gc_port_for_can_hub(&can_hub0, conn_fd);
+    stack.connect_tcp_gridconnect_hub("localhost", 12021);
 
-    LoggingBit logger(EVENT_ID, EVENT_ID + 1, "blinker");
-    nmranet::BitEventConsumer consumer(&logger);
-    BlinkerFlow blinker(&g_node);
-
-    // Bootstraps the alias allocation process.
-    g_if_can.alias_allocator()->send(g_if_can.alias_allocator()->alloc());
-
-    AutomataRunner runner(&g_node, automata_code);
-    resume_all_automata();
-
-    g_executor.thread_body();
+    stack.loop_executor();
     return 0;
 }
