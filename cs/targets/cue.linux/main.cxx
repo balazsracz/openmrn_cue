9378fe6f9333ea5c3c3317b95414a558efc35ce9
Balazs Racz <bracz@google.com>
2015-09-23 11:55:22 +0200

    Updates linux cue node to re-try connecting to the main server.

--- cs/targets/cue.linux/main.cxx
+++ cs/targets/cue.linux/main.cxx
@@ -34,6 +34,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h> /* tc* functions */
 
 #include "nmranet/SimpleStack.hxx"
 #include "nmranet/SimpleNodeInfoMockUserFile.hxx"
@@ -47,8 +48,8 @@ using nmranet::DatagramClient;
 static const nmranet::NodeID NODE_ID = 0x05010101143EULL;
 nmranet::SimpleCanStack stack(NODE_ID);
 
-nmranet::MockSNIPUserFile snip_user_file("Signal logic",
-                                         "This node is loaded with the signaling logic");
+nmranet::MockSNIPUserFile snip_user_file(
+    "Signal logic", "This node is loaded with the signaling logic");
 const char *const nmranet::SNIP_DYNAMIC_FILENAME =
     nmranet::MockSNIPUserFile::snip_user_file_path;
 
@@ -97,18 +98,117 @@ void parse_args(int argc, char *argv[]) {
 
 extern char automata_code[];
 
-bracz_custom::AutomataControl automatas(stack.node(), stack.dg_service(), (const insn_t*) automata_code);
+bracz_custom::AutomataControl automatas(stack.node(), stack.dg_service(),
+                                        (const insn_t *)automata_code);
 
+class ConnectionClient {
+ public:
+  /** Test the connection whether it is alive; establish the connection if it
+   * is dead. */
+  virtual void ping() = 0;
+};
+
+class DeviceClosedNotify : public Notifiable {
+ public:
+  DeviceClosedNotify(int *fd, string name) : fd_(fd), name_(name) {}
+  void notify() override {
+    LOG_ERROR("Connection to %s closed.", name_.c_str());
+    *fd_ = -1;
+  }
+
+ private:
+  int *fd_;
+  string name_;
+};
+
+class FdConnectionClient : public ConnectionClient {
+ public:
+  FdConnectionClient(const string &name) : closedNotify_(&fd_, name) {}
+
+  void ping() OVERRIDE {
+    if (fd_ < 0) {
+      try_connect();
+    }
+  }
+
+ protected:
+  virtual void try_connect() = 0;
+
+  int fd_{-1};
+  DeviceClosedNotify closedNotify_;
+};
+
+class DeviceConnectionClient : public FdConnectionClient {
+ public:
+  DeviceConnectionClient(const string &name, const string &dev)
+      : FdConnectionClient(name), dev_(dev) {}
+
+ private:
+  void try_connect() OVERRIDE {
+    fd_ = ::open(dev_.c_str(), O_RDWR);
+    if (fd_ >= 0) {
+      // Sets up the terminal in raw mode. Otherwise linux might echo
+      // characters coming in from the device and that will make
+      // packets go back to where they came from.
+      HASSERT(!tcflush(fd_, TCIOFLUSH));
+      struct termios settings;
+      HASSERT(!tcgetattr(fd_, &settings));
+      cfmakeraw(&settings);
+      cfsetspeed(&settings, B115200);
+      HASSERT(!tcsetattr(fd_, TCSANOW, &settings));
+      LOG(INFO, "Opened device %s.\n", device_path);
+      create_gc_port_for_can_hub(stack.can_hub(), fd_, &closedNotify_);
+    } else {
+      LOG_ERROR("Failed to open device %s: %s\n", device_path, strerror(errno));
+      fd_ = -1;
+    }
+  }
+
+  string dev_;
+};
+
+class UpstreamConnectionClient : public FdConnectionClient {
+ public:
+  UpstreamConnectionClient(const string &name, const string &host, int port)
+      : FdConnectionClient(name), host_(host), port_(port) {}
+
+ private:
+  void try_connect() OVERRIDE {
+    fd_ = ConnectSocket(host_.c_str(), port_);
+    if (fd_ >= 0) {
+      LOG_ERROR("Connected to %s:%d\n", host_.c_str(), port_);
+      create_gc_port_for_can_hub(stack.can_hub(), fd_, &closedNotify_);
+    } else {
+      LOG_ERROR("Failed to connect to %s:%d: %s\n", host_.c_str(), port_,
+                strerror(errno));
+      fd_ = -1;
+    }
+  }
+
+  string host_;
+  int port_;
+};
 
 int appl_main(int argc, char *argv[]) {
   parse_args(argc, argv);
 
+  vector<std::unique_ptr<ConnectionClient>> connections;
+
   if (device_path) {
-    stack.add_gridconnect_port(device_path);
+    connections.emplace_back(new DeviceConnectionClient("device", device_path));
   } else {
-    stack.connect_tcp_gridconnect_hub(host, port);
+    connections.emplace_back(
+        new UpstreamConnectionClient("upstream", host, port));
   }
 
-  stack.loop_executor();
+  stack.start_executor_thread("stack", 0, 0);
+
+  while (1) {
+    for (const auto &p : connections) {
+      p->ping();
+    }
+    sleep(1);
+  }
+
   return 0;
 }
