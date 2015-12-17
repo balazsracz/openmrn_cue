8c507ccc2b0a4112f5ffa99b8532cccbdc62154f
Balazs Racz <bracz@google.com>
2015-12-17 22:08:32 +0100

    work on creating the correct business logic for railcom

--- cs/targets/railcom/main.cxx
+++ cs/targets/railcom/main.cxx
@@ -196,43 +196,158 @@ void set_output_disable(unsigned port, bool enable) {
 
 class PortLogic : public StateFlowBase {
  public:
-  PortLogic(uint8_t channel)
+  PortLogic(uint8_t channel, int config_fd, )
       : StateFlowBase(stack.service()), channel_(channel) {
     start_flow(STATE(init_wait));
   }
 
   void set_overcurrent(bool value) {
     if (value) {
-      // enable ptr is inverted.
-      enable_ptrs[channel_]->set();
+      // We have a short circuit. Stop the output.
+      set_output_enable(false);
+      killedOutputDueToOvercurrent_ = 1;
+      if (is_terminated()) {
+        start_flow(STATE(test_again));
+      }
     }
-    inOvercurrent_ = value ? 1 : 0;
+  }
+
+  void set_occupancy(bool value) {
+    sensedOccupancy_ = to_u(value);
     if (is_terminated()) {
       start_flow(STATE(test_again));
     }
   }
 
-  void set_occupancy(bool value) {
-    inOccupancy_ = value ? 1 : 0;
+  void set_network_occupancy(bool value) {
+    networkOccupancy_ = to_u(value);
+    if (is_terminated()) {
+      start_flow(STATE(test_again));
+    }
+  }
+
+  void set_network_overcurrent(bool value) {
+    networkOvercurrent_ = to_u(value);
+    if (is_terminated()) {
+      start_flow(STATE(test_again));
+    }
+  }
+
+  void set_network_enable(bool value) {
+    networkEnable_ = to_u(value);
     if (is_terminated()) {
       start_flow(STATE(test_again));
     }
   }
 
  private:
+  static const int SHORT_RETRY_WAIT_MSEC = 300;
+  static const int SHORT_RETRY_COUNT = 3;
+
+  unsigned to_u(bool b) {
+    return b ? 1 : 0;
+  }
+
+  /// @param value is true if the output should be provided with power, false
+  /// if the output should have no power.
+  void set_output_enable(bool value) {
+    commandedEnable_ = to_u(value);
+    enable_ptrs[channel_]->write(!value); // inverted
+    if (value) {
+      killedOutputDueToOvercurrent_ = to_u(false);
+    }
+  }
+
   Action init_wait() {
     return sleep_and_call(&timer_, MSEC_TO_NSEC(100 * channel_),
                           STATE(init_try_turnon));
   }
 
-  Action init_try_turnon() { return set_terminated(); }
+  Action init_try_turnon() {
+    turnonTryCount_ = 0;
+    return call_immediately(STATE(try_turnon));
+  }
 
-  Action test_again() { return set_terminated(); }
+  Action try_turnon() {
+    set_output_enable(true);
+    return sleep_and_call(&timer, MSEC_TO_NSEC(SHORT_RETRY_WAIT_MSEC), STATE(eval_turnon));
+  }
+
+  Action eval_turnon() {
+    if (killedOutputDueToOvercurrent_) {
+      // Turnon failed. try again.
+      if (turnonTryCount_ < SHORT_RETRY_COUNT) {
+        turnonTryCount_++;
+        return call_immediately(try_turnon);
+      }
+      // Turnon failed, no more tries left.
+      if (!networkOvercurrent_ || isInitialTurnon_) {
+        networkOvercurrent_ = to_u(true);
+        return produce_event(eventOvercurrentOn_);
+      }
+      // Network already knows we are in overcurrent -- Do nothing.
+      return call_immediately(STATE(test_again));
+    }
+    // now: not overcurrent -- we're successfully on.
+    if (networkOvercurrent_ || isInitialTurnon_) {
+      networkOvercurrent_ = to_u(false);
+      return produce_event(eventOvercurrentOff_);
+    }
+    return call_immediately(STATE(test_again));
+  }
+
+  // Evaluates that the network-visible states are the same as the locally
+  // sensed state.
+  Action test_again() {
+    if (commandedEnable_) {
+      turnonTryCount_ = 0;
+      // If we have track power, we produce occupancy events.
+      if (networkOccupancy_ != sensedOccupancy_) {
+        networkOccupancy_ = sensedOccupancy_;
+        if (sensedOccupancy_) {
+          return produce_event(eventOccupancyOn_);
+        } else {
+          return produce_event(eventOccupancyOff_);
+        }
+      }
+    }
+
+    if (isInitialTurnon_) {
+      isInitialTurnon_ = 0;
+    }
+    return set_terminated();
+  }
+
+
+  Action produce_event(nmranet::EventID event_id) {
+    writer->WriteAsync(node_, Defs::MTI_EVENT_REPORT, WriteHelper::global(),
+                       eventid_to_buffer(event_id), n_.reset(this));
+    return wait_and_call(STATE(test_again));
+  }
 
   StateFlowTimer timer_{this};
   uint8_t channel_;
-  uint8_t inOvercurrent_ : 1;
-  uint8_t inOccupancy_ : 1;
+  /// set-once when overcurrent is detected.
+  uint8_t killedOutputDueToOvercurrent_ : 1;
+  uint8_t sensedOccupancy_ : 1;    //< Current (debounced) sensor read
+  uint8_t networkOvercurrent_ : 1; //< Current network state
+  uint8_t networkOccupancy_ : 1;   //< Current network state
+  uint8_t commandedEnable_ : 1;    //< Current output pin state
+  uint8_t networkEnable_ : 1;      //< Current network state
+  uint8_t isInitialTurnon_ : 1;    //< true only for the first turnon try.
+  uint8_t isShorted_ : 1;    //< true when we are shut down due to short
+
+  /// how many tries we had yet for turning on output.
+  uint8_t turnonTryCount_ : 3;
+  Node* node_;
+  nmranet::EventID eventOccupancyOn_;
+  nmranet::EventID eventOccupancyOff_;
+  nmranet::EventID eventOvercurrentOn_;
+  nmranet::EventID eventOvercurrentOff_;
+  nmranet::EventID eventEnableOn_;
+  nmranet::EventID eventEnableOff_;
+  nmranet::WriteHelper h_;
+  BarrierNotifiable n_;
 };
 
 template <class Debouncer>
