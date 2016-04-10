c10c010b33248e722d93852038e0d8e1eddca08c
Balazs Racz <balazs.racz@gmail.com>
2016-04-10 21:00:46 +0200

    Makes memorize reconnect to the hub automatically when it loses the connection.

--- cs/targets/memorize.linux.x86/main.cxx
+++ cs/targets/memorize.linux.x86/main.cxx
@@ -38,9 +38,9 @@
 #include "os/os.h"
 
 #include "custom/MemorizingEventHandler.hxx"
-#include "nmranet/SimpleStack.hxx"
 #include "nmranet/SimpleNodeInfoMockUserFile.hxx"
-
+#include "nmranet/SimpleStack.hxx"
+#include "utils/ClientConnection.hxx"
 
 static const nmranet::NodeID NODE_ID = 0x050101011442ULL;
 nmranet::SimpleCanStack stack(NODE_ID);
@@ -48,21 +48,82 @@ nmranet::SimpleCanStack stack(NODE_ID);
 nmranet::MockSNIPUserFile snip_user_file(
     "Memorizing node",
     "Provides persistent state information for train location events.");
-const char* const nmranet::SNIP_DYNAMIC_FILENAME =
+const char *const nmranet::SNIP_DYNAMIC_FILENAME =
     nmranet::MockSNIPUserFile::snip_user_file_path;
+
+extern const nmranet::SimpleNodeStaticValues nmranet::SNIP_STATIC_DATA = {
+    4, "Balazs Racz", "Memorizing node", "linux.x86", "1.2"};
 
 static const uint64_t BRACZ_LAYOUT = 0x0501010114FF0000ULL;
 
 nmranet::MemorizingHandlerManager g_permabits(stack.node(),
                                               BRACZ_LAYOUT | 0xC000, 1024, 2);
 
+void usage(const char *e) {
+  fprintf(stderr,
+          "Usage: %s [-p port] [-d device_path] [-u upstream_host] "
+          "[-q upstream_port] [-t]\n\n",
+          e);
+  fprintf(stderr,
+          "Memorizing node. Keeps state of a certain set of events in RAM, "
+          "providing prior state information for nodes that reboot on the "
+          "OpenLCB network.\n\nArguments:\n");
+  fprintf(stderr,
+          "\t-u upstream_host   is the host name for a GridConnect TCP "
+          "hub. Required.\n");
+  fprintf(stderr,
+          "\t-q upstream_port   is the port number for the GC hub. Default "
+          "12021.\n");
+  exit(1);
+}
+
+int upstream_port = 12021;
+const char *upstream_host = nullptr;
+
+void parse_args(int argc, char *argv[]) {
+  int opt;
+  while ((opt = getopt(argc, argv, "hu:q:")) >= 0) {
+    switch (opt) {
+      case 'h':
+        usage(argv[0]);
+        break;
+      case 'u':
+        upstream_host = optarg;
+        break;
+      case 'q':
+        upstream_port = atoi(optarg);
+        break;
+      default:
+        fprintf(stderr, "Unknown option %c\n", opt);
+        usage(argv[0]);
+    }
+  }
+  if (!upstream_host) {
+    fprintf(stderr, "Must specify -u for upstream host.\n");
+    usage(argv[0]);
+  }
+}
+
 /** Entry point to application.
  * @param argc number of command line arguments
  * @param argv array of command line arguments
  * @return 0, should never return
  */
-int appl_main(int argc, char* argv[]) {
-  stack.connect_tcp_gridconnect_hub("localhost", 12021);
-  stack.loop_executor();
+int appl_main(int argc, char *argv[]) {
+  parse_args(argc, argv);
+
+  std::unique_ptr<ConnectionClient> connection(new UpstreamConnectionClient(
+      "hub", stack.can_hub(), upstream_host, upstream_port));
+
+  while (!connection->ping()) {
+    sleep(1);
+  }
+  stack.start_executor_thread("nmranet_exec", 0, 0);
+
+  while (1) {
+    connection->ping();
+    sleep(1);
+  }
+
   return 0;
 }
