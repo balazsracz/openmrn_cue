1a5eba7cd5675fed0ec497a8700899e34ab62b41
Balazs Racz <balazs.racz@gmail.com>
2016-04-21 09:09:51 +0200

    updates reboot script to accept longer address too

--- scripts/train-reboot.sh
+++ scripts/train-reboot.sh
@@ -3,5 +3,7 @@
 source $(dirname $0)/train-base.sh
 
 export DST=$1
-
-train-send-datagram -n 0x0501010114$DST  -g 20A9
+if [ ${#DST} == 2 ]; then
+    DST=0x0501010114$DST
+fi
+train-send-datagram -n $DST  -g 20A9
