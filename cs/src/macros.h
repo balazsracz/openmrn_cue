d94a58050eb26f64c427507674f8217a88a559e6
Balazs Racz <balazs.racz@gmail.com>
2016-06-30 08:12:44 -0500

    Fix build.

--- cs/src/macros.h
+++ cs/src/macros.h
@@ -2,10 +2,6 @@
 #ifndef _BRACZ_TRAIN_MACROS_H_
 #define _BRACZ_TRAIN_MACROS_H_
 
-
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&);   \
-  void operator=(const TypeName&)
-
+#include "utils/macros.h"
 
 #endif  //_BRACZ_TRAIN_MACROS_H_
