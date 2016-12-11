94ca002d8ec078c3955313ce94903215f803725d
Balazs Racz <balazs.racz@gmail.com>
2016-12-11 16:08:31 +0100

    Adds a query to get back automata state.

--- cs/custom/AutomataControl.hxx
+++ cs/custom/AutomataControl.hxx
@@ -2,31 +2,38 @@
 #ifndef _BRACZ_CUSTOM_AUTOMATACONTROL_HXX_
 #define _BRACZ_CUSTOM_AUTOMATACONTROL_HXX_
 
-#include "src/automata_runner.h"
 #include "nmranet/DatagramHandlerDefault.hxx"
+#include "nmranet/Defs.hxx"
+#include "src/automata_runner.h"
 
 namespace bracz_custom {
 
 struct AutomataDefs {
   enum {
     DATAGRAM_CODE = 0xF0,
+    RESPONSE_CODE = 0xF1,
 
     STOP_AUTOMATA = 0x10,
     RESTART_AUTOMATA = 0x11,
+
+    GET_AUTOMATA_STATE = 0x20,
+
+    ERROR_AUTOMATA_NOT_FOUND = nmranet::Defs::ERROR_INVALID_ARGS | 0xF,
   };
 };
 
 class AutomataControl : public nmranet::DefaultDatagramHandler {
  public:
-
-  AutomataControl(nmranet::Node* node, nmranet::DatagramService* if_datagram, const insn_t* code)
-      : DefaultDatagramHandler(if_datagram),
-        runner_(node, code) {
-    dg_service()->registry()->insert(runner_.node(), AutomataDefs::DATAGRAM_CODE, this);
+  AutomataControl(nmranet::Node* node, nmranet::DatagramService* if_datagram,
+                  const insn_t* code)
+      : DefaultDatagramHandler(if_datagram), runner_(node, code) {
+    dg_service()->registry()->insert(runner_.node(),
+                                     AutomataDefs::DATAGRAM_CODE, this);
   }
 
   ~AutomataControl() {
-    dg_service()->registry()->erase(runner_.node(), AutomataDefs::DATAGRAM_CODE, this);
+    dg_service()->registry()->erase(runner_.node(), AutomataDefs::DATAGRAM_CODE,
+                                    this);
   }
 
   Action entry() OVERRIDE {
@@ -34,7 +41,8 @@ class AutomataControl : public nmranet::DefaultDatagramHandler {
       return respond_reject(nmranet::DatagramClient::PERMANENT_ERROR);
     }
     uint8_t cmd = payload()[1];
-    switch(cmd) {
+    needResponse_ = 0;
+    switch (cmd) {
       case AutomataDefs::STOP_AUTOMATA: {
         runner_.Stop(this, false);
         return wait_and_call(STATE(stop_done));
@@ -43,19 +51,74 @@ class AutomataControl : public nmranet::DefaultDatagramHandler {
         runner_.Start();
         return respond_ok(0);
       }
+      case AutomataDefs::GET_AUTOMATA_STATE: {
+        if (size() < 4) {
+          return respond_reject(
+              nmranet::Defs::ERROR_INVALID_ARGS_MESSAGE_TOO_SHORT);
+        }
+        automataNum_ = payload()[2];
+        automataNum_ <<= 8;
+        automataNum_ |= payload()[3];
+        if (automataNum_ >= runner_.GetAllAutomatas().size()) {
+          return respond_reject(AutomataDefs::ERROR_AUTOMATA_NOT_FOUND);
+        }
+        responsePayload_.clear();
+        responsePayload_.push_back(AutomataDefs::RESPONSE_CODE);
+        responsePayload_.push_back(cmd);
+        responsePayload_.push_back(payload()[2]);
+        responsePayload_.push_back(payload()[3]);
+        responsePayload_.push_back(
+            runner_.GetAllAutomatas()[automataNum_]->GetState());
+        needResponse_ = 1;
+        return respond_ok(nmranet::DatagramDefs::REPLY_PENDING);
+      }
       default:
         return respond_reject(nmranet::DatagramClient::PERMANENT_ERROR);
     }
   }
 
-  Action stop_done() {
-    return respond_ok(0);
+  Action ok_response_sent() override {
+    if (!needResponse_) return release_and_exit();
+    return allocate_and_call(STATE(client_allocated),
+                             dg_service()->client_allocator());
   }
+
+  Action client_allocated() {
+    clientFlow_ = full_allocation_result(dg_service()->client_allocator());
+    return allocate_and_call(dg_service()->iface()->dispatcher(),
+                             STATE(send_response_datagram));
+  }
+
+  Action send_response_datagram() {
+    auto* b = get_allocation_result(dg_service()->iface()->dispatcher());
+    b->set_done(b_.reset(this));
+    b->data()->reset(nmranet::Defs::MTI_DATAGRAM,
+                     message()->data()->dst->node_id(), message()->data()->src,
+                     nmranet::EMPTY_PAYLOAD);
+    b->data()->payload.swap(responsePayload_);
+    clientFlow_->write_datagram(b);
+    return wait_and_call(STATE(wait_response_datagram));
+  }
+
+  Action wait_response_datagram() {
+    if (clientFlow_->result() & nmranet::DatagramClient::OPERATION_PENDING) {
+      DIE("Unexpected notification from the datagram client.");
+    }
+    dg_service()->client_allocator()->typed_insert(clientFlow_);
+    return release_and_exit();
+  }
+
+  Action stop_done() { return respond_ok(0); }
 
  private:
   AutomataRunner runner_;
+  nmranet::DatagramClient* clientFlow_;
+  nmranet::DatagramPayload responsePayload_;
+  uint16_t automataNum_;
+  uint16_t needResponse_ : 1;
+  BarrierNotifiable b_;
 };
 
-} // namespace bracz_custom
+}  // namespace bracz_custom
 
-#endif // _BRACZ_CUSTOM_AUTOMATACONTROL_HXX_
+#endif  // _BRACZ_CUSTOM_AUTOMATACONTROL_HXX_
