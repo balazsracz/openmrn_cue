d70a5f8332c0bf07758be78ea33d93abab41b374
Balazs Racz <balazs.racz@gmail.com>
2016-12-16 21:01:46 +0100

    Reconstructs symlinks.

--- cs/targets/acc.tiva.2/NodeId.cxx
+++ cs/targets/acc.tiva.2/NodeId.cxx
@@ -1,5 +1,1 @@
-#include "openlcb/If.hxx"
-#include "address.h"
-
-extern const openlcb::NodeID NODE_ID;
-const openlcb::NodeID NODE_ID = 0x050101011400ULL | NODEID_LOW_BITS;
+../acc.tiva.3/NodeId.cxx
