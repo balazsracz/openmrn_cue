8d135c7507e95bb28b69328bb554fb66891d5a13
Balazs Racz <balazs.racz@gmail.com>
2017-12-28 19:13:59 +0100

    Adds implementation of prioritized update loop and unittests for it.

--- cs/commandstation/UpdateProcessor.hxx
+++ cs/commandstation/UpdateProcessor.hxx
@@ -60,11 +60,23 @@ class UpdateProcessor : public StateFlow<Buffer<dcc::Packet>, QList<1> >,
   ~UpdateProcessor();
 
   /** Adds a new refresh source to the background refresh packets. */
-  void add_refresh_source(dcc::PacketSource* source) OVERRIDE {
+  void add_refresh_source(dcc::PacketSource* source,
+                          unsigned priority) OVERRIDE {
     AtomicHolder h(this);
     refreshSources_.push_back(source);
     hasRefreshSource_ = 1;
-    lastPacketTime_[source] = 0;
+    auto& s = packetSourceStates_[source];
+    s.priority_ = priority;
+    s.lastPacketTime_ = 0;
+    if (priority >= EXCLUSIVE_MIN_PRIORITY) {
+      unsigned last_priority = 0;
+      if (exclusiveIndex_ != NO_EXCLUSIVE) {
+        last_priority = packetSourceStates_[refreshSources_[exclusiveIndex_]].priority_;
+      }
+      if (priority > last_priority) {
+        exclusiveIndex_ = refreshSources_.size() - 1;
+      }
+    }
   }
   /** Deletes a packet refresh source. */
   void remove_refresh_source(dcc::PacketSource* source) OVERRIDE {
@@ -75,7 +87,18 @@ class UpdateProcessor : public StateFlow<Buffer<dcc::Packet>, QList<1> >,
     if (refreshSources_.empty()) {
       hasRefreshSource_ = 0;
     }
-    lastPacketTime_.erase(source);
+    packetSourceStates_.erase(source);
+    // Recomputes which is the largest priority and whether we have exclusive.
+    unsigned max_priority = EXCLUSIVE_MIN_PRIORITY;
+    unsigned max_index = NO_EXCLUSIVE;
+    for (unsigned i = 0; i < refreshSources_.size(); ++i) {
+      const auto& st = packetSourceStates_[refreshSources_[i]];
+      if (st.priority_ >= max_priority) {
+        max_index = i;
+        max_priority = st.priority_;
+      }
+    }
+    exclusiveIndex_ = max_index;
   }
 
   /** Notifies that a packet source has an urgent packet. */
@@ -85,22 +108,42 @@ class UpdateProcessor : public StateFlow<Buffer<dcc::Packet>, QList<1> >,
   Action entry() OVERRIDE;
 
  private:
-  // Place where we forward the packets filled in.
+  struct SourceState {
+    /// Stores the last time we sent a packet to a given loco. Suppresses
+    /// refresh packet if this time is too recent to avoid confusing DCC
+    /// decoders.
+    long long lastPacketTime_;
+    /// Priority of packet source. This also encodes whether this is an
+    /// exclusive packet source.
+    unsigned priority_;
+  };
+
+  /// @return if we have an exclusive source.
+  bool has_exclusive() {
+    return exclusiveIndex_ != NO_EXCLUSIVE;
+  }
+  
+  /// Place where we forward the packets filled in.
   dcc::PacketFlowInterface* trackSend_;
 
-  // Holds the list of train nodes that have reported a change. We will always
-  // take a train node from this list first before starting background refresh.
+  /// Holds the list of train nodes that have reported a change. We will always
+  /// take a train node from this list first before starting background refresh.
   QList<1> priorityUpdates_;
 
-  // Packet sources to ask about refreshing data periodically.
+  /// Packet sources to ask about refreshing data periodically.
   vector<dcc::PacketSource*> refreshSources_;
 
-  // Stores the last time we sent a packet to a given loco. Suppresses refresh
-  // packet if this time is too recent to avoid confusing DCC decoders.
-  map<dcc::PacketSource*, long long> lastPacketTime_;
-
-  // Which is the next guy on the refresh source list to add.
-  unsigned nextRefreshIndex_ : 16;
+  /// Stores additional data about each packet source.
+  map<dcc::PacketSource*, SourceState> packetSourceStates_;
+  
+  /// Which is the next guy on the refresh source list to add.
+  unsigned nextRefreshIndex_ : 15;
+  /// The highest priority refresh index, if we have a exclusive index.
+  unsigned exclusiveIndex_ : 15;
+  /// This is stored in the exclusiveIndex_ when we have no exclusive refresh
+  /// source.
+  static constexpr unsigned NO_EXCLUSIVE = 0x7FF;
+  /// Non-zero if we have any refresh sources.
   unsigned hasRefreshSource_ : 1;
 };
 
