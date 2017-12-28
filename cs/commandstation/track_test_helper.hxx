85e6ecf270c9d3a56fb68c259a4e4110a268b5ec
Balazs Racz <balazs.racz@gmail.com>
2017-12-28 21:03:02 +0100

    Refactors the test infrastructure to a separate file to be usable by the estop test.

---
+++ cs/commandstation/track_test_helper.hxx
@@ -0,0 +1,49 @@
+#include "utils/async_if_test_helper.hxx"
+
+#include "commandstation/UpdateProcessor.hxx"
+#include "dcc/Loco.hxx"
+#include "dcc/UpdateLoop.hxx"
+
+#include "dcc/dcc_test_utils.hxx"
+
+using ::testing::ElementsAre;
+
+class MockPacketQueue : public dcc::PacketFlowInterface {
+ public:
+  MOCK_METHOD1(arrived, void(const dcc::Packet&));
+
+  void send(Buffer<dcc::Packet>* b, unsigned prio) {
+    dcc::Packet* pkt = b->data();
+    arrived(*pkt);
+    b->unref();
+  }
+};
+
+OVERRIDE_CONST(dcc_packet_min_refresh_delay_ms, 50);
+
+class UpdateProcessorTest : public AsyncCanTest {
+ protected:
+  UpdateProcessorTest() : updateProcessor_(&g_service, &trackSendQueue_) {}
+
+  ~UpdateProcessorTest() { wait(); }
+
+  void send_empty_packet() {
+    Buffer<dcc::Packet>* b;
+    mainBufferPool->alloc(&b, nullptr);
+    updateProcessor_.send(b);
+  }
+
+  void wait() {
+    usleep(70000);
+    AsyncCanTest::wait();
+    Mock::VerifyAndClear(&updateProcessor_);
+  }
+
+  void qwait() {
+    AsyncCanTest::wait();
+    Mock::VerifyAndClear(&updateProcessor_);
+  }
+
+  StrictMock<MockPacketQueue> trackSendQueue_;
+  commandstation::UpdateProcessor updateProcessor_;
+};
