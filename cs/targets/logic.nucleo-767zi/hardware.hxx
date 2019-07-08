30eefd43b04c4429d13d428b81ec8da056d23dda
Balazs Racz <balazs.racz@gmail.com>
2019-07-08 00:27:47 -0600

    Adds generated makefiles.

--- cs/targets/logic.nucleo-767zi/hardware.hxx
+++ cs/targets/logic.nucleo-767zi/hardware.hxx
@@ -1,1 +1,17 @@
-../../../../openmrn/boards/st-stm32f767zi-nucleo/hardware.hxx
+
+#include "Stm32Gpio.hxx"
+#include "utils/GpioInitializer.hxx"
+#include "BlinkerGPIO.hxx"
+
+GPIO_PIN(LED1_RAW, LedPin, B, 0);
+GPIO_PIN(LED2, LedPin, B, 7);
+GPIO_PIN(LED3, LedPin, B, 14);
+
+GPIO_PIN(SW_USER, GpioInputPU, C, 13);
+
+typedef GpioInitializer<LED1_RAW_Pin, LED2_Pin, LED3_Pin, SW_USER_Pin> GpioInit;
+
+typedef LED1_RAW_Pin BLINKER_RAW_Pin;
+typedef BLINKER_Pin LED1_Pin;
+
+typedef LED2_Pin LED_GREEN_Pin;
