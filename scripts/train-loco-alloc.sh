9071cff3adaf20f5dd3da620008e79a8d963bfad
Balazs Racz <balazs.racz@gmail.com>
2020-09-05 21:59:28 +0200

    Adds a second parameter to define where to start allocating.

--- scripts/train-loco-alloc.sh
+++ scripts/train-loco-alloc.sh
@@ -3,7 +3,8 @@
 source $(dirname $0)/train-base.sh
 
 NUM=${1:-1}
-for adr in $(seq 300 $((300 - 1 + $NUM))) ; do
+START=${2:-0}
+for adr in $(seq $((300 + $START)) $((300 - 1 + $NUM))) ; do
     sendpkt ":X19914575N090099FFFF$(printf %04d $adr)C0;"
     sleep 0.2
 done
