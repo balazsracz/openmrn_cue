87fe54f983411efdf0882f0a4d29e783f7f09993
Balazs Racz <balazs.racz@gmail.com>
2022-01-22 17:11:57 +0100

    Reformat cs.js. (#42)

--- cs/targets/cs.js/main.cxx
+++ cs/targets/cs.js/main.cxx
@@ -35,22 +35,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "commandstation/UpdateProcessor.hxx"
-#include "mobilestation/MobileStationTraction.hxx"
-#include "commandstation/TrainDb.hxx"
 #include "commandstation/AllTrainNodes.hxx"
-#include "openlcb/SimpleNodeInfoMockUserFile.hxx"
-#include "openlcb/SimpleStack.hxx"
-#include "openlcb/TractionTestTrain.hxx"
-#include "openlcb/TractionTrain.hxx"
+#include "commandstation/TrainDb.hxx"
+#include "commandstation/UpdateProcessor.hxx"
+#include "custom/LoggingBit.hxx"
 #include "dcc/FakeTrackIf.hxx"
 #include "executor/PoolToQueueFlow.hxx"
-#include "custom/LoggingBit.hxx"
+#include "mobilestation/MobileStationTraction.hxx"
+#include "openlcb/SimpleNodeInfoMockUserFile.hxx"
+#include "openlcb/SimpleStack.hxx"
 #include "openlcb/TractionCvSpace.hxx"
-#include "utils/JSTcpClient.hxx"
+#include "openlcb/TractionTestTrain.hxx"
+#include "openlcb/TractionTrain.hxx"
 #include "utils/JSSerialPort.hxx"
+#include "utils/JSTcpClient.hxx"
 #include "utils/JSTcpHub.hxx"
-
 
 static const openlcb::NodeID NODE_ID = 0x050101011440ULL;
 openlcb::SimpleCanStack stack(NODE_ID);
@@ -65,14 +64,30 @@ OVERRIDE_CONST(gc_generate_newlines, 1);
 namespace commandstation {
 extern const struct const_traindb_entry_t const_lokdb[];
 const struct const_traindb_entry_t const_lokdb[] = {
-  // 0
-  //  { 51, { LIGHT, TELEX, FN_NONEXISTANT, SHUNT, ABV },
-  //  "BR 260417", DCC_28 },  // ESU LokPilot 3.0
-  // 1
-  //{ 66, { LIGHT },
-  //"Re 6/6 11665", DCC_128 },
-  { 31, { LIGHT, FN_NONEXISTANT, TELEX, ABV ,}, "F7 31", DCCMODE_FAKE_DRIVE },
-  { 123, { LIGHT, FN_NONEXISTANT, TELEX, ABV ,}, "Example 123", DCC_128 },
+    // 0
+    //  { 51, { LIGHT, TELEX, FN_NONEXISTANT, SHUNT, ABV },
+    //  "BR 260417", DCC_28 },  // ESU LokPilot 3.0
+    // 1
+    //{ 66, { LIGHT },
+    //"Re 6/6 11665", DCC_128 },
+    {31,
+     {
+         LIGHT,
+         FN_NONEXISTANT,
+         TELEX,
+         ABV,
+     },
+     "F7 31",
+     DCCMODE_FAKE_DRIVE},
+    {123,
+     {
+         LIGHT,
+         FN_NONEXISTANT,
+         TELEX,
+         ABV,
+     },
+     "Example 123",
+     DCC_128},
 
     {7000, {0xff}, "Node test 7000", DCC_28},
     {5000, {0xff}, "TT 5000", DCC_28},
@@ -80,8 +95,18 @@ const struct const_traindb_entry_t const_lokdb[] = {
     {5100, {0xff}, "TT 5100", DCC_28},
     {13, {0xff}, "MM 13", MARKLIN_NEW},
     {15, {0xff}, "MM 15", MARKLIN_NEW},
-  { 0, {0,}, "", 0},
-  { 0, {0,}, "", 0},
+    {0,
+     {
+         0,
+     },
+     "",
+     0},
+    {0,
+     {
+         0,
+     },
+     "",
+     0},
 };
 extern const size_t const_lokdb_size;
 const size_t const_lokdb_size = sizeof(const_lokdb) / sizeof(const_lokdb[0]);
@@ -93,8 +118,7 @@ int upstream_port = 12021;
 const char *upstream_host = nullptr;
 bool print_packets = false;
 
-void usage(const char *e)
-{
+void usage(const char *e) {
   fprintf(stderr,
           "Usage: %s [-l] [-p port] [-d device_path] [-u upstream_host [-q "
           "upstream_port]]\n\n",
@@ -117,49 +141,46 @@ void usage(const char *e)
   exit(1);
 }
 
-void parse_args(int argc, char *argv[])
-{
-    int opt;
-    while ((opt = getopt(argc, argv, "hp:u:q:d:lD")) >= 0)
-    {
-        switch (opt)
-        {
-            case 'h':
-                usage(argv[0]);
-                break;
-            case 'd':
-                device_paths.push_back(optarg);
-                break;
-            case 'D':
-                JSSerialPort::list_ports();
-                break;
-            case 'p':
-                port = atoi(optarg);
-                break;
-            case 'u':
-                upstream_host = optarg;
-                break;
-            case 'q':
-                upstream_port = atoi(optarg);
-                break;
-            case 'l':
-                print_packets = true;
-                break;
-            default:
-                fprintf(stderr, "Unknown option %c\n", opt);
-                usage(argv[0]);
-        }
+void parse_args(int argc, char *argv[]) {
+  int opt;
+  while ((opt = getopt(argc, argv, "hp:u:q:d:lD")) >= 0) {
+    switch (opt) {
+      case 'h':
+        usage(argv[0]);
+        break;
+      case 'd':
+        device_paths.push_back(optarg);
+        break;
+      case 'D':
+        JSSerialPort::list_ports();
+        break;
+      case 'p':
+        port = atoi(optarg);
+        break;
+      case 'u':
+        upstream_host = optarg;
+        break;
+      case 'q':
+        upstream_port = atoi(optarg);
+        break;
+      case 'l':
+        print_packets = true;
+        break;
+      default:
+        fprintf(stderr, "Unknown option %c\n", opt);
+        usage(argv[0]);
     }
+  }
 }
 
-openlcb::MockSNIPUserFile snip_user_file("Fake CS",
-                                         "");
-const char* const openlcb::SNIP_DYNAMIC_FILENAME =
+openlcb::MockSNIPUserFile snip_user_file("Fake CS", "");
+const char *const openlcb::SNIP_DYNAMIC_FILENAME =
     openlcb::MockSNIPUserFile::snip_user_file_path;
 
 dcc::FakeTrackIf track_if(stack.service(), 2);
 commandstation::UpdateProcessor cs_loop(stack.service(), &track_if);
-PoolToQueueFlow<Buffer<dcc::Packet>> pool_translator(stack.service(), track_if.pool(), &cs_loop);
+PoolToQueueFlow<Buffer<dcc::Packet>> pool_translator(stack.service(),
+                                                     track_if.pool(), &cs_loop);
 
 LoggingBit track_on_off(openlcb::Defs::EMERGENCY_OFF_EVENT,
                         openlcb::Defs::CLEAR_EMERGENCY_OFF_EVENT,
@@ -169,17 +190,21 @@ openlcb::BitEventConsumer powerbit(&track_on_off);
 openlcb::TrainService traction_service(stack.iface());
 
 commandstation::TrainDb train_db;
-commandstation::AllTrainNodes all_train_nodes(&train_db, &traction_service, stack.info_flow(), stack.memory_config_handler());
+commandstation::AllTrainNodes all_train_nodes(&train_db, &traction_service,
+                                              stack.info_flow(),
+                                              stack.memory_config_handler());
 
 dcc::RailcomHubFlow railcom_hub(stack.service());
-openlcb::TractionCvSpace traction_cv(stack.memory_config_handler(), &track_if, &railcom_hub, openlcb::MemoryConfigDefs::SPACE_DCC_CV);
+openlcb::TractionCvSpace traction_cv(stack.memory_config_handler(), &track_if,
+                                     &railcom_hub,
+                                     openlcb::MemoryConfigDefs::SPACE_DCC_CV);
 
 /** Entry point to application.
  * @param argc number of command line arguments
  * @param argv array of command line arguments
  * @return 0, should never return
  */
-int appl_main(int argc, char* argv[]) {
+int appl_main(int argc, char *argv[]) {
   parse_args(argc, argv);
   std::unique_ptr<JSTcpHub> hub;
   if (port >= 0) {
